5fd7df7bede42bdfb423efcfc0930406e92c6cfd
Teruteru <teruterubouzu1024@gmail.com>
2018-01-31 16:19:21 +0900

    Format import statements
    Update gitignore

--- test/com/twitter/teruteru128/test/holders/VoidHolderTest.java
+++ test/com/twitter/teruteru128/test/holders/VoidHolderTest.java
@@ -1,8 +1,7 @@
 package com.twitter.teruteru128.test.holders;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
