4911290d88813c18f1e8eb181ec859cf52951160
Teruteru <teruterubouzu1024@gmail.com>
2018-02-08 00:22:33 +0900

    Add certificate reading function method
    Separate the certificate reading function into another method
    Add numerical expansion processing

--- src/main/java/com/twitter/teruteru128/test/rsa/sample001/Main.java
+++ src/main/java/com/twitter/teruteru128/test/rsa/sample001/Main.java
@@ -19,18 +19,36 @@ public class Main {
 	 */
 	public static void main(String[] args) throws CertificateException,
 			IOException {
-		CertificateFactory factory = CertificateFactory.getInstance("X.509");
-		try (InputStream stream = ClassLoader
-				.getSystemResourceAsStream("verisign_G5.cer")) {
-			Certificate certificate = factory.generateCertificate(stream);
-			RSAPublicKey key = (RSAPublicKey) certificate.getPublicKey();
-			BigInteger modulus = key.getModulus();
-			System.out.printf("0x%0514x%n", modulus);
+		Certificate certificate = loadCertificate("verisign_G5.cer");
+		RSAPublicKey key = (RSAPublicKey) certificate.getPublicKey();
+		BigInteger modulus = key.getModulus();
+		System.out.printf("0x%0514x%n", modulus);
+		// 2, 4, 8, 16
+		// 3, 5, 7, 9, 11, 13
+		int[][] matrix = { { 2, 7 }, { 5, 8 }, { 3, 13 }, { 4, 9 }, { 11, 16 } };
+		int length = matrix.length;
+		BigInteger tmp = modulus;
+		for (int i = 0; i < length; i++) {
+			int[] map = matrix[i];
+			tmp = convert(tmp, map[0], map[1]);
 		}
+		System.out.printf("0x%0514x%n", tmp);
 	}
 
 	public static BigInteger convert(BigInteger n, int out, int in) {
 		return new BigInteger(n.toString(out), in);
 	}
 
+	/**
+	 * 
+	 */
+	private static Certificate loadCertificate(String name)
+			throws CertificateException, IOException {
+		CertificateFactory factory = CertificateFactory.getInstance("X.509");
+		try (InputStream stream = ClassLoader.getSystemResourceAsStream(name)) {
+			return factory.generateCertificate(stream);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
