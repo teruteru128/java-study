1197cbc33631c6e8030d01c813c96433c33e547c
Teruteru <teruterubouzu1024@gmail.com>
2018-03-12 18:51:25 +0900

    Delete call of conversion method

--- src/main/java/com/twitter/teruteru128/test/rsa/sample001/Main.java
+++ src/main/java/com/twitter/teruteru128/test/rsa/sample001/Main.java
@@ -23,18 +23,6 @@ public class Main {
 		RSAPublicKey key = (RSAPublicKey) certificate.getPublicKey();
 		BigInteger modulus = key.getModulus();
 		System.out.printf("0x%x%n", modulus);
-		// 2, 4, 8, 16
-		// 3, 5, 7, 9, 11, 13
-		// 2, 3, 4, 5, 7, 8, 9, 11, 13, 16
-		// 7, 8, 5, 9, 4, 11, 3, 13, 2, 16
-		int[][] matrix = { { 2, 3 }, { 4, 5 }, { 7, 8 }, { 9, 11 }, { 13, 16 } };
-		int length = matrix.length;
-		BigInteger tmp = modulus;
-		for (int i = 0; i < length; i++) {
-			int[] map = matrix[i];
-			tmp = convert(tmp, map[0], map[1]);
-		}
-		System.out.printf("0x%x%n", tmp);
 	}
 
 	public static BigInteger convert(BigInteger n, int out, int in) {
