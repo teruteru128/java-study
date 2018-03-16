cd823b0b3670628dd9446397a684a8153f59b564
Teruteru <teruterubouzu1024@gmail.com>
2018-03-16 18:25:04 +0900

    Optimization
    Remove creation of new object

--- src/main/java/com/twitter/teruteru128/test/ts/sample003/Main.java
+++ src/main/java/com/twitter/teruteru128/test/ts/sample003/Main.java
@@ -20,14 +20,15 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		String publicKey = "MEwDAgcAAgEgAiEA7Vo1+Orf2xuuu6hTPAPldSfrUZZ7WYAzpRcO5DoYFLoCIF1JKVBctOGvMOy495O/BWFuFEYH4i1f6vU0b9+a64RD";
-		SecureRandom random=SecureRandomHolder.getInstance();
+		SecureRandom random = SecureRandomHolder.getInstance();
 		int min = 38;
 		ArrayList<Callable<Void>> list = new ArrayList<>();
-		for (int i = 0; i < 4; i++) {
-			list.add(Executors.callable(new Searcher(publicKey, random.nextLong(), min),
+		for (int i = 0; i < 6; i++) {
+			list.add(Executors.callable(
+					new Searcher(publicKey, random.nextLong(), min),
 					VoidHolder.getInstance()));
 		}
-		ExecutorService service = Executors.newFixedThreadPool(2);
+		ExecutorService service = Executors.newWorkStealingPool();
 		try {
 			service.invokeAll(list);
 		} catch (InterruptedException e) {
--- src/main/java/com/twitter/teruteru128/test/ts/sample003/Searcher.java
+++ src/main/java/com/twitter/teruteru128/test/ts/sample003/Searcher.java
@@ -33,40 +33,43 @@ class Searcher implements Runnable {
 		try {
 			MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
 			long verifier = this.verifier;
-			String verifier_str;
 			byte[] buf = new byte[20];
-			final int length = publicKey.length();
+			final byte[] key = publicKey.getBytes();
 			final int minbytes = this.minbytes;
 			final int minbits = this.minSecurityLevel;
-			StringBuilder buffer = new StringBuilder(publicKey);
 			int zerobytes;
 			int zerobits;
 			int securityLevel = 0;
+			final boolean flg = (minSecurityLevel < 8);
+			int length;
 			while (true) {
-				zerobytes = zerobits = 0;
-				verifier_str = Long.toUnsignedString(verifier, 10);
-				buffer.append(verifier_str);
-				sha1.update(buffer.toString().getBytes());
-				buffer.setLength(length);
+				length = toUnsignedDecByteArray(verifier, buf);
+				sha1.update(key);
+				sha1.update(buf, 0, length);
 				try {
 					sha1.digest(buf, 0, 20);
 				} catch (DigestException e) {
 					e.printStackTrace();
 				}
-				for (zerobytes = 0; zerobytes < 20 && buf[zerobytes] == 0; zerobytes++) {
-					// NONE
-				}
-				if (zerobytes >= minbytes) {
-					if (zerobytes < 20) {
-						byte lastbyte = buf[zerobytes];
-						for (zerobits = 0; (lastbyte & 1) != 1; zerobits++, lastbyte >>= 1) {
-							// NONE
-						}
-						securityLevel = 8 * zerobytes + zerobits;
-						if (securityLevel >= minbits) {
-							System.out.printf(
-									"Security Level(verifier) : %d(%s)%n",
-									securityLevel, verifier_str);
+				if (flg || buf[0] == 0) {
+					for (zerobytes = 0; zerobytes < 20 && buf[zerobytes] == 0; zerobytes++) {
+						// NONE
+					}
+					if (zerobytes >= minbytes) {
+						if (zerobytes < 20) {
+							byte lastbyte = buf[zerobytes];
+							for (zerobits = 0; (lastbyte & 1) != 1; zerobits++, lastbyte >>= 1) {
+								// NONE
+							}
+							securityLevel = 8 * zerobytes + zerobits;
+							if (securityLevel >= minbits) {
+								synchronized (System.out) {
+									System.out
+											.printf("Security Level(verifier) : %d(%s)%n",
+													securityLevel,
+													Long.toUnsignedString(verifier));
+								}
+							}
 						}
 					}
 				}
@@ -77,4 +80,95 @@ class Searcher implements Runnable {
 		}
 	}
 
+	private static final byte[] DIGIT = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9' };
+
+	/**
+	 * 
+	 */
+	private static int toUnsignedDecByteArray(long l, byte[] buf) {
+		long quot = (l >>> 1) / 5;
+		long rem = l - quot * 10;
+		int length = 0;
+		if (l == Long.MIN_VALUE) {
+			System.arraycopy("18446744073709551615".getBytes(), 0, buf, 0, 20);
+			length = 20;
+		} else if (l >= 0) {
+			length = toUnsignedDecByteArrayPos(l, buf);
+		} else {
+			length = toUnsignedDecByteArrayPos(quot, buf);
+			buf[++length] = DIGIT[(int) rem];
+		}
+		return length;
+	}
+
+	private static final byte[] DigitTens = { '0', '0', '0', '0', '0', '0',
+			'0', '0', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+			'1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '3', '3',
+			'3', '3', '3', '3', '3', '3', '3', '3', '4', '4', '4', '4', '4',
+			'4', '4', '4', '4', '4', '5', '5', '5', '5', '5', '5', '5', '5',
+			'5', '5', '6', '6', '6', '6', '6', '6', '6', '6', '6', '6', '7',
+			'7', '7', '7', '7', '7', '7', '7', '7', '7', '8', '8', '8', '8',
+			'8', '8', '8', '8', '8', '8', '9', '9', '9', '9', '9', '9', '9',
+			'9', '9', '9', };
+	private static final byte[] DigitOnes = { '0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+			'9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1',
+			'2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7',
+			'8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+			'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3',
+			'4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9', };
+	private static final byte[] digits = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9' };
+
+	private static int toUnsignedDecByteArrayPos(long l, byte[] buf) {
+		int length = stringSize(l);
+		long q;
+		int r;
+		int charPos = length;
+		while (l > Integer.MAX_VALUE) {
+			q = l / 100;
+			// really: r = i - (q * 100);
+			r = (int) (l - ((q << 6) + (q << 5) + (q << 2)));
+			l = q;
+			buf[--charPos] = DigitOnes[r];
+			buf[--charPos] = DigitTens[r];
+		}
+		int q2;
+		int i2 = (int) l;
+		while (i2 >= 65536) {
+			q2 = i2 / 100;
+			r = i2 - ((q2 << 6) + (q2 << 5) + (q2 << 2));
+			i2 = q2;
+			buf[--charPos] = DigitOnes[r];
+			buf[--charPos] = DigitTens[r];
+		}
+		for (;;) {
+			q2 = (i2 * 52429) >>> (16 + 3);
+			r = i2 - ((q2 << 3) + (q2 << 1));
+			buf[--charPos] = digits[r];
+			i2 = q2;
+			if (i2 == 0) {
+				break;
+			}
+		}
+		return length;
+	}
+
+	/**
+	 * @param l
+	 * @return
+	 */
+	private static int stringSize(long l) {
+		long p = 10;
+		for (int i = 1; i < 19; i++) {
+			if (l < p) {
+				return i;
+			}
+			p = 10 * p;
+		}
+		return 19;
+	}
 }
