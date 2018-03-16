3bc9d32cbe600b2579a11f22a8a261b0d88748ed
Teruteru <teruterubouzu1024@gmail.com>
2018-03-16 20:21:30 +0900

    Clean up try-catch statements

--- src/main/java/com/twitter/teruteru128/holders/VoidHolder.java
+++ src/main/java/com/twitter/teruteru128/holders/VoidHolder.java
@@ -1,7 +1,6 @@
 package com.twitter.teruteru128.holders;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author Teruteru
@@ -23,18 +22,8 @@ public final class VoidHolder {
 					.getDeclaredConstructor(new Class<?>[0]);
 			constructor.setAccessible(true);
 			instance = constructor.newInstance(new Object[0]);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
+		} catch (Throwable e) {
+			throw new InternalError(e);
 		} finally {
 			INSTANCE = instance;
 		}
