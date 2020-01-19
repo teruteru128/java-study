0420f76146d6606d25f9a76879bd0af82dfee89a
Yuta Onose <onose999999999@gmail.com>
2020-01-19 17:20:09 +0900

    Update

---
+++ src/main/java/ColorCodeTest.java
@@ -0,0 +1,6 @@
+public
+class ColorCodeTest{
+  public static void main(String[]args){
+    System.out.println("ひぐらしの\u001b[31mな\u001b[0mく頃に");
+  }
+}
---
+++ src/main/java/DateStudy.java
@@ -0,0 +1,15 @@
+
+import java.time.*;
+import java.util.*;
+
+class DateStudy{
+
+  public static void main(String[] args){
+    OffsetDateTime time1 = OffsetDateTime.parse("2038-01-19T03:14:07Z");
+    OffsetDateTime time2 = OffsetDateTime.parse("2038-01-19T03:14:08Z");
+    System.out.println(time1);
+    System.out.println(time2);
+    Date date2=Date.from(time2.toInstant());
+    System.out.println(date2);
+  }
+}
