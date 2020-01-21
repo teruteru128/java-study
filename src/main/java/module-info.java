9abc28ff59666c17ff81ee3855d8a38a2832f5ac
Yuta Onose <onose999999999@gmail.com>
2020-01-21 11:51:45 +0900

    Sort module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,18 +4,18 @@
  *
  */
 module com.twitter.teruteru128.study {
-	requires static org.bouncycastle.provider;
-	requires static java.xml.bind;
-	requires static org.twitter4j.core;
-	requires static java.logging;
-//	requires static java.json;
-	requires static org.json;
 	requires static java.desktop;
-	requires static java.sql;
-	requires static org.bukkit;
-	requires static jcuda;
-	requires static org.bouncycastle.pg;
+//	requires static java.json;
+	requires static java.logging;
 	requires static java.management;
 	requires static java.scripting;
+	requires static java.sql;
+	requires static java.xml.bind;
 	requires static com.google.gson;
+	requires static jcuda;
+	requires static org.bouncycastle.pg;
+	requires static org.bouncycastle.provider;
+	requires static org.bukkit;
+	requires static org.json;
+	requires static org.twitter4j.core;
 }
