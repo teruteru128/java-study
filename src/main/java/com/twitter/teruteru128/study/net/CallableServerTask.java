88736aed475855f63d34eacd6efd2a1b01e1eeb5
Teruteru <teruterubouzu1024@gmail.com>
2020-01-28 17:56:12 +0900

    Update server study

---
+++ src/main/java/com/twitter/teruteru128/study/net/CallableServerTask.java
@@ -0,0 +1,13 @@
+package com.twitter.teruteru128.study.net;
+
+import java.util.concurrent.Callable;
+
+class CallableServerTask implements Callable<Void> {
+	public CallableServerTask() {
+	}
+
+	@Override
+	public Void call() throws Exception {
+		return null;
+	}
+}
