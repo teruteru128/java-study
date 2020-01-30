d40b0bdcdf916bec981b7615b99341dbe343380d
Teruteru <teruterubouzu1024@gmail.com>
2020-01-30 16:00:36 +0900

    Rename local file read from url sample

--- src/main/java/com/twitter/teruteru128/study/ReadLocalFileViaURLSample.java
+++ src/main/java/com/twitter/teruteru128/study/ReadLocalFileViaURLSample.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-class Main {
+public class ReadLocalFileViaURLSample {
 
   public static void main(String[] args) throws Exception {
     URL url = new URL("file:pom.xml");
