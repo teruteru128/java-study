401cc1e4163f0d624d5044ca4ae632a5f6c8fd5b
Teruteru <teruterubouzu1024@gmail.com>
2020-01-31 09:43:23 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/SumOfThreeCubes.java
+++ src/main/java/com/twitter/teruteru128/study/SumOfThreeCubes.java
@@ -1,26 +1,16 @@
 package com.twitter.teruteru128.study;
-import static java.lang.Math.abs;
-import static java.math.BigInteger.valueOf;
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.util.List;
 
-class SumOfThreeCubes{
+class SumOfThreeCubes {
 
-  public static void main(String args[]){
-    Random random = new Random(System.nanoTime());
-    BigInteger x = valueOf(abs(random.nextLong())), y = valueOf(abs(random.nextLong())), z = valueOf(abs(random.nextLong()));
-    System.out.printf("x = %d, y = %d, z = %d\n",  x, y, z);
-    BigInteger x3 = x.pow(3), y3 = y.pow(3), z3 = z.pow(3);
-    //System.out.printf("%d\n",  x3.add(y3).add(z3));
-    System.out.printf("%d\n",  x3.add(y3).subtract(z3));
-    System.out.printf("%d\n",  x3.subtract(y3).add(z3));
-    System.out.printf("%d\n",  x3.subtract(y3).subtract(z3));
-    System.out.printf("%d\n",  x3.negate().add(y3).add(z3));
-    System.out.printf("%d\n",  x3.negate().add(y3).subtract(z3));
-    System.out.printf("%d\n",  x3.negate().subtract(y3).add(z3));
-    System.out.printf("%d\n",  x3.negate().subtract(y3).subtract(z3));
-    return;
+  public static void main(String args[]) {
+    var sum = BigInteger.valueOf(0);
+    List<BigInteger> terms = List.of(new BigInteger("569936821221962380720"), new BigInteger("-569936821113563493509"), new BigInteger("-472715493453327032"));
+    for (BigInteger term : terms) {
+      sum = sum.add(term.pow(3));
+    }
+    System.out.println(sum);
   }
 }
-
