cfec05d94301f8aa7cd48d475c05a5aa69e9c891
Teruteru <teruterubouzu1024@gmail.com>
2020-02-01 15:26:35 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/serialize/sample001/Main.java
+++ src/main/java/com/twitter/teruteru128/study/serialize/sample001/Main.java
@@ -1,11 +1,11 @@
 package com.twitter.teruteru128.study.serialize.sample001;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-
-import javax.xml.bind.DatatypeConverter;
+import java.util.concurrent.Callable;
 
 /**
  * @author Teruteru
@@ -16,19 +16,33 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		try (ObjectOutputStream oos = new ObjectOutputStream(stream)) {
+	public static void main(String[] args) throws Exception {
+		ByteArrayOutputStream stream = null;
+		try (ObjectOutputStream oos = new ObjectOutputStream(stream = new ByteArrayOutputStream())) {
 			oos.writeObject(new Serializable() {
-				/**
-				 * 
-				 */
 				private static final long serialVersionUID = 1L;
 			});
-		} catch (IOException e) {
-			e.printStackTrace();
+			oos.writeObject((Runnable & Serializable) () -> {
+				System.out.println("うんこー！");
+			});
 		}
-		System.out.println(DatatypeConverter.printHexBinary(stream.toByteArray()));
+		try (ObjectInputStream oin = new ObjectInputStream(new ByteArrayInputStream(stream.toByteArray()))) {
+			Serializable serializable = null;
+			for (int i = 0; i < 4 && (serializable = (Serializable) oin.readObject()) != null; i++) {
+				System.out.printf("%s, %s%n", serializable, serializable.getClass());
+				if (serializable instanceof Runnable) {
+					Runnable r = (Runnable) serializable;
+					r.run();
+				}
+				if (serializable instanceof Callable<?>) {
+					Callable<?> r = (Callable<?>) serializable;
+					var k = r.call();
+					if (k instanceof String) {
+						System.out.println(k);
+					}
+				}
+			}
+		}
 	}
 
 }
