fc07100e4341c7f8b274308cb75730fd33f59f60
Teruteru <teruterubouzu1024@gmail.com>
2020-02-01 22:24:45 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/tcp/server/Main.java
+++ src/main/java/com/twitter/teruteru128/study/tcp/server/Main.java
@@ -4,19 +4,31 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Main {
+public class Main implements Runnable {
 
 	public Main() {
 	}
 
-	public static void main(String[] args) throws Exception {
+	@Override
+	public void run() {
 		ExecutorService service = Executors.newWorkStealingPool();
 		String hostname = "localhost";
 		int port = 50000;
 		CountDownLatch latch = new CountDownLatch(1);
 		ServerConfig config = new ServerConfig(hostname, port, service, latch);
 		service.submit(new ServerWaittingThread(config));
-		config.getLatch().await();
+		try {
+			config.getLatch().await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			service.shutdown();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread serverThread = new Thread(new Main());
+		serverThread.start();
 	}
 
 }
