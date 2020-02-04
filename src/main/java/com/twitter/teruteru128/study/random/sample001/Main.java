8fb51527092a9f48272c3b2a48a119d4e5f1c460
Teruteru <teruterubouzu1024@gmail.com>
2020-02-04 13:25:23 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/random/sample001/Main.java
+++ src/main/java/com/twitter/teruteru128/study/random/sample001/Main.java
@@ -1,5 +1,7 @@
 package com.twitter.teruteru128.study.random.sample001;
 
+import java.sql.SQLException;
+
 import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
@@ -9,12 +11,22 @@ import com.mysql.cj.jdbc.MysqlDataSource;
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		MysqlDataSource dataSource = new MysqlDataSource();
-		MysqlConnectionPoolDataSource dataSource2 = new MysqlConnectionPoolDataSource();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws SQLException {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost/wordpress");
+        dataSource.setUser("wordpressuser");
+        dataSource.setPassword("password");
+        try (var connection = dataSource.getConnection()) {
+        }
+        MysqlConnectionPoolDataSource dataSource2 = new MysqlConnectionPoolDataSource();
+        dataSource2.setURL("jdbc:mysql://localhost/wordpress");
+        dataSource2.setUser("wordpressuser");
+        dataSource2.setPassword("password");
+        try (var connection = dataSource2.getConnection()) {
+        }
+    }
 
 }
