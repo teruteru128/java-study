a5b6fdc95680badf21b0bca1aa48bbc934cf0d82
Teruteru <teruterubouzu1024@gmail.com>
2020-02-06 11:08:55 +0900

    Move mysql sample

--- src/main/java/com/twitter/teruteru128/study/random/sample001/Main.java
+++ src/main/java/com/twitter/teruteru128/study/random/sample001/Main.java
@@ -1,10 +1,5 @@
 package com.twitter.teruteru128.study.random.sample001;
 
-import java.sql.SQLException;
-
-import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
-import com.mysql.cj.jdbc.MysqlDataSource;
-
 /**
  * @author Teruteru
  *
@@ -14,19 +9,7 @@ public class Main {
     /**
      * @param args
      */
-    public static void main(String[] args) throws SQLException {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setURL("jdbc:mysql://localhost/wordpress");
-        dataSource.setUser("wordpressuser");
-        dataSource.setPassword("password");
-        try (var connection = dataSource.getConnection()) {
-        }
-        MysqlConnectionPoolDataSource dataSource2 = new MysqlConnectionPoolDataSource();
-        dataSource2.setURL("jdbc:mysql://localhost/wordpress");
-        dataSource2.setUser("wordpressuser");
-        dataSource2.setPassword("password");
-        try (var connection = dataSource2.getConnection()) {
-        }
+    public static void main(String[] args) {
     }
 
 }
