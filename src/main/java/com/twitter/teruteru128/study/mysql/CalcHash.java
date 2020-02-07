94de822187b77a4d9c0319d2d9266aea1b2bc6a7
Teruteru <teruterubouzu1024@gmail.com>
2020-02-07 12:28:27 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/mysql/CalcHash.java
+++ src/main/java/com/twitter/teruteru128/study/mysql/CalcHash.java
@@ -2,17 +2,24 @@ package com.twitter.teruteru128.study.mysql;
 
 import java.security.MessageDigest;
 
-public class CalcHash{
+public class CalcHash {
 
+    private static byte MIN_CHAR = 33;
+    private static byte MAX_CHAR = 126;
+
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         String password = "advanforce";
         MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
         var hash = sha1.digest(sha1.digest(password.getBytes()));
         final var c = "0123456789ABCDEF".toCharArray();
         StringBuilder sb = new StringBuilder(41).append('*');
-        for(byte b : hash){
-            sb.append(c[(b>>4) & 0x0f]);
-            sb.append(c[(b>>0) & 0x0f]);
+        for (byte b : hash) {
+            sb.append(c[(b >> 4) & 0x0f]);
+            sb.append(c[(b >> 0) & 0x0f]);
         }
         System.out.println(sb.toString());
     }
