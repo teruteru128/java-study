38b5733173c96a0f6c637b3d31279b2e64604bb2
Teruteru <teruterubouzu1024@gmail.com>
2020-02-14 09:07:17 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/rsa/PrimeSearch.java
+++ src/main/java/com/twitter/teruteru128/study/rsa/PrimeSearch.java
@@ -9,25 +9,32 @@ import java.nio.file.StandardOpenOption;
 
 public class PrimeSearch implements Runnable {
 
-    public PrimeSearch() {
+    private String inputName;
+    private String outputName;
+
+    public PrimeSearch(String resourceName, String out) {
         super();
+        this.inputName = resourceName;
+        this.outputName = out;
     }
 
     public static void main(String[] args) {
-        Thread thread = new Thread(new PrimeSearch());
-        thread.start();
+        Thread thread1 = new Thread(new PrimeSearch("prime1.bin", "probablePrime1.bin"));
+        thread1.start();
+        Thread thread2 = new Thread(new PrimeSearch("prime2.bin", "probablePrime2.bin"));
+        thread2.start();
     }
 
     @Override
     public void run() {
         try {
-            var primefileURL = ClassLoader.getSystemResource("prime1.bin");
+            var primefileURL = ClassLoader.getSystemResource(inputName);
             var primefileURI = primefileURL.toURI();
             var primefilePath = Paths.get(primefileURI);
             var primeBytes = Files.readAllBytes(primefilePath);
             BigInteger a = new BigInteger(1, primeBytes, 0, primeBytes.length);
             var probablePrime = a.nextProbablePrime();
-            Files.write(Paths.get("probablePrime1.bin"), probablePrime.toByteArray(), StandardOpenOption.WRITE,
+            Files.write(Paths.get(outputName), probablePrime.toByteArray(), StandardOpenOption.WRITE,
                     StandardOpenOption.CREATE);
         } catch (URISyntaxException e) {
             e.printStackTrace();
