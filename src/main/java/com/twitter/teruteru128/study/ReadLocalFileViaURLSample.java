ab6ae6d2e5bc5b0cef49b59f3644fedace4fa2fb
Teruteru <teruterubouzu1024@gmail.com>
2020-03-06 13:49:44 +0900

    Remove

--- src/main/java/com/twitter/teruteru128/study/ReadLocalFileViaURLSample.java
+++ src/main/java/com/twitter/teruteru128/study/ReadLocalFileViaURLSample.java
@@ -12,6 +12,5 @@ public class ReadLocalFileViaURLSample {
     try (var reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
       reader.lines().forEach(System.out::println);
     }
-    System.out.println("/home/teruteru128/Documents/ある新婚夫婦の平凡な日常.txt");
   }
 }
