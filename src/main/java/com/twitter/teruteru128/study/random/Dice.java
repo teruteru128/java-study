0f026ea47813af6d112ee013710b143d1d245042
Teruteru <teruterubouzu1024@gmail.com>
2020-03-24 10:54:58 +0900

    Update Dice

--- src/main/java/com/twitter/teruteru128/study/random/Dice.java
+++ src/main/java/com/twitter/teruteru128/study/random/Dice.java
@@ -4,9 +4,38 @@ import java.security.SecureRandom;
 
 public class Dice {
 
+    /**
+     * 115
+229
+358
+52
+189
+263
+129
+292
+121
+263
+294
+245
+120
+211
+234
+98
+223
+50
+110
+124
+46
+268
+
+     * @param args
+     * @throws Exception
+    */
     public static void main(final String[] args) throws Exception {
         final var random = SecureRandom.getInstance("nativeprngnonblocking");
-        System.out.printf("%03d%n", random.nextInt(999) + 1);
+        System.out.printf("%d%n", random.nextInt(360));
+        random.ints(24, 0, 360).forEach(System.out::println);
+        random.doubles(16).map(d->1/d).forEach(System.out::println);
     }
 
 }
