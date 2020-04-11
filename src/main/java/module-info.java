43e7b51af569e5fd7837ac7f4ef763af2d7de242
Teruteru <teruterubouzu1024@gmail.com>
2020-04-12 00:48:02 +0900

    Update pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@ module com.twitter.teruteru128.study {
 //    requires java.json;
     requires java.logging;
     requires java.management;
+    requires java.naming;
+    requires java.rmi;
     requires java.scripting;
     requires java.sql;
     requires java.xml.bind;
@@ -20,4 +22,9 @@ module com.twitter.teruteru128.study {
     requires org.twitter4j.core;
     requires mysql.connector.java;
     requires commons.math3;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.h2database;
+    requires jakarta.mail;
+    requires twitter.text;
 }
