0ca00d97b201480bc6f658995a686b2d922a0029
Teruteru <teruterubouzu1024@gmail.com>
2020-04-13 17:22:53 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/swing/Main.java
+++ src/main/java/com/twitter/teruteru128/study/swing/Main.java
@@ -1,19 +1,13 @@
 package com.twitter.teruteru128.study.swing;
 
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import org.h2.jdbcx.JdbcDataSource;
 
 /**
  * @author Teruteru
@@ -31,7 +25,7 @@ public class Main {
         jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
         jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         
-        BufferedImage img = ImageIO.read(new File("167887.jpg"));
+        BufferedImage img = ImageIO.read(ClassLoader.getSystemResource("167887.jpg"));
         if (img != null) {
             JLabel l=new JLabel(new ImageIcon(img));
             jf.add(l);
--- src/main/java/com/twitter/teruteru128/test/jndi/DNSSample.java
+++ src/main/java/com/twitter/teruteru128/test/jndi/DNSSample.java
@@ -1,5 +1,6 @@
 package com.twitter.teruteru128.test.jndi;
 
+import java.util.Hashtable;
 import java.util.Properties;
 
 import javax.naming.Context;
@@ -11,28 +12,38 @@ import javax.naming.directory.InitialDirContext;
 
 public class DNSSample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String name = "google.co.jp";
-        Properties props = new Properties();
+        Hashtable<Object, Object> props = new Properties();
         props.put(Context.INITIAL_CONTEXT_FACTORY,
                 "com.sun.jndi.dns.DnsContextFactory");
-        props.put(Context.PROVIDER_URL, "dns://192.168.0.1");
+        props.put(Context.PROVIDER_URL, "dns://192.168.0.1/");
+        // こっちにも付けられる
+        // props.put(Context.PROVIDER_URL, "dns://192.168.0.1/" + name);
         try {
             InitialDirContext idctx = new InitialDirContext(props);
-            Attributes attrs = idctx.getAttributes(name);
+            Attributes attrs = idctx.getAttributes(name, new String[]{ "AAAA" });
             NamingEnumeration<? extends Attribute> allAttr = attrs.getAll();
             while (allAttr.hasMore()) {
                 Attribute attr = allAttr.next();
                 System.out.println("Attribute: " + attr.getID());
                 NamingEnumeration<?> values = attr.getAll();
                 while (values.hasMore()) {
-                    Object tmp =values.next();
-                    System.out.print("Value: " + tmp.getClass());
-                    System.out.println(", Value: " + tmp);
+                    Object tmp = values.next();
+                    System.out.print("\t");
+                    System.out.println(tmp);
                 }
             }
         } catch (NamingException e) {
             e.printStackTrace();
         }
+        Hashtable<Object, Object> env = new Hashtable<>();
+        env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+        env.put("java.naming.provider.url",    "dns://192.168.1.1/sun.com");
+        var ictx = new InitialDirContext(env);
+        Attributes attrs1 = ictx.getAttributes("", new String[] {"A"});
+        System.out.println(attrs1);
+        Attributes attrs2 = ictx.getAttributes("", new String[] {"MX"});
+        System.out.println(attrs2);
     }
 }
