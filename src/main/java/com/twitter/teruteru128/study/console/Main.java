7b0deb4f00ea7acf8b2d35013927fcc54e6585ca
Teruteru <teruterubouzu1024@gmail.com>
2020-04-13 12:57:16 +0900

    Format

--- src/main/java/com/twitter/teruteru128/study/console/Main.java
+++ src/main/java/com/twitter/teruteru128/study/console/Main.java
@@ -12,27 +12,26 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-          Console console = System.console();
-          if(console==null){
-              System.out.println("console is null");
-              throw new NullPointerException("Console not Found!");
-          }
-          for (int i = 0;; i++) {
-              char[] password = 
-                  console.readPassword("パスワード %d: ", i);
-              if (password == null) {
-                  break;
-              }
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("console is null");
+            throw new NullPointerException("Console not Found!");
+        }
+        for (int i = 0;; i++) {
+            char[] password = console.readPassword("パスワード %d: ", i);
+            if (password == null) {
+                break;
+            }
 
-              console.printf("パスワード: ");
-              for (int j = 0; j < password.length; j++) {
-                  char c = password[j];
-                  console.printf("%c", c);
+            console.printf("パスワード: ");
+            for (int j = 0; j < password.length; j++) {
+                char c = password[j];
+                console.printf("%c", c);
 
-                  password[j] = '\0';
-              }
-              console.printf("%n");
-          }
+                password[j] = '\0';
+            }
+            console.printf("%n");
+        }
 
     }
 
