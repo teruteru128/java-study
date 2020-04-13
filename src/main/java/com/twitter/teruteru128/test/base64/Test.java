fa02f7ea66868da0393ed67c85fcd40bd5442deb
Teruteru <teruterubouzu1024@gmail.com>
2020-04-13 11:27:36 +0900

    Fix line separator

--- src/main/java/com/twitter/teruteru128/test/base64/Test.java
+++ src/main/java/com/twitter/teruteru128/test/base64/Test.java
@@ -2,6 +2,7 @@ package com.twitter.teruteru128.test.base64;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.nio.charset.Charset;
 import java.util.Base64;
 
 /**
@@ -15,7 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         byte[] buffer = new byte[2048];
-        Base64.Encoder encoder = Base64.getMimeEncoder(76, null);
+        Base64.Encoder encoder = Base64.getMimeEncoder(76, System.lineSeparator().getBytes(Charset.forName("UTF-8")));
         int readed_length;
         try (BufferedInputStream bin = new BufferedInputStream(System.in);BufferedOutputStream bout = new BufferedOutputStream(encoder.wrap(System.out))) {
             while((readed_length = bin.read(buffer,0,buffer.length)) != -1){
