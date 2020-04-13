2c4e31abf4311dee2e84d41ce97bc2c7ae187e83
Teruteru <teruterubouzu1024@gmail.com>
2020-04-13 16:33:06 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/password/Main.java
+++ src/main/java/com/twitter/teruteru128/study/password/Main.java
@@ -11,7 +11,7 @@ import javax.crypto.spec.PBEKeySpec;
 public class Main {
     /**
      * 鍵長(定数)
-     * */
+     */
     private static final int KEY_LENGTH = 512;
 
     public static void main(String[] args) {
@@ -23,17 +23,9 @@ public class Main {
         byte[] salt = Base64.getDecoder().decode(base64_salt);
         /**
          * ストレッチング繰り返し回数(引数)
-         * */
-        int iterationCount = 0b11_1111_1111_1111;
-        /*
-         * salt = new byte[] { 46, -84, -92, -32, -32, 98, 68, 102, -96, -121,
-         * -43, 67, 105, -68, -7, 54, -95, 85, 116, -46, -39, -58, 15, 76, -92,
-         * 86, -105, 124, -93, 109, -20, -42 };
          */
-        System.out.println("hi!1");
-        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount,
-                KEY_LENGTH);
-        System.out.println("hi!2");
+        int iterationCount = (1 << 14) - 1;
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, KEY_LENGTH);
         SecretKeyFactory factory = null;
         try {
             factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
@@ -41,22 +33,20 @@ public class Main {
             e.printStackTrace();
         } finally {
         }
-        System.out.println("hi!3");
         SecretKey secretKey = null;
-        long start = System.currentTimeMillis();
-        for(int i=0;i<100;i++){
-        try {
-            secretKey = factory.generateSecret(keySpec);
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
+        long start = System.nanoTime();
+        final int loopCount = 100;
+        for (int i = 0; i < loopCount; i++) {
+            try {
+                secretKey = factory.generateSecret(keySpec);
+            } catch (InvalidKeySpecException e) {
+                e.printStackTrace();
+            }
         }
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("合計: "+(end-start)+"ミリ秒");
-        System.out.println("1回あたり: "+(end-start)/100d+"ミリ秒");
-        System.out.println("hi!4");
-        String hashedPassword = Base64.getEncoder().encodeToString(
-                secretKey.getEncoded());
+        long end = System.nanoTime();
+        System.out.printf("合計: %.6f秒%n", (end - start) / 1000000000d);
+        System.out.printf("1回あたり: %.10f秒%n", (end - start) / (loopCount * 1000000000d));
+        String hashedPassword = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         System.out.println(hashedPassword);
     }
 }
