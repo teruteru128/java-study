ecc1f8be126f10cdafdc38858d5ed2b430d4e13c
Teruteru <teruterubouzu1024@gmail.com>
2020-04-14 11:38:19 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/net/Test.java
+++ src/main/java/com/twitter/teruteru128/study/net/Test.java
@@ -5,17 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.zip.GZIPInputStream;
 
 public class Test {
 
-    public static void main(String[] args) throws MalformedURLException, URISyntaxException, IOException {
-        String str = null;
+    public static void main(String[] args) throws MalformedURLException, IOException {
         HttpURLConnection.setFollowRedirects(true);
-        URL url = new URI("http://www.yahoo.co.jp/").toURL();
+        URL url = new URL("http://www.yahoo.co.jp/");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("User-Agent",
                 "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0");
@@ -26,9 +23,6 @@ public class Test {
         con.setInstanceFollowRedirects(true);
         con.connect();
         int responseCode = con.getResponseCode();
-        String responseMessage = con.getResponseMessage();
-        System.out.println(responseCode);
-        System.out.println(responseMessage);
         boolean redirect = false;
         if (responseCode != HttpURLConnection.HTTP_OK && (responseCode == HttpURLConnection.HTTP_MOVED_TEMP
                 || responseCode == HttpURLConnection.HTTP_MOVED_PERM
@@ -43,16 +37,14 @@ public class Test {
             con.setRequestProperty("Cache-Control", "max-age=0");
             con.setRequestProperty("Accept-Language", "ja,en-US;q=0.7,en;q=0.3");
             con.setRequestProperty("Accept-Encoding", "gzip,deflate");
+            System.out.println(con.getClass());
             con.connect();
         }
-        System.out.println(con.getHeaderField("Content-Encoding"));
+        String contentEncoding = con.getHeaderField("Content-Encoding");
         try (BufferedReader urlIn = new BufferedReader(new InputStreamReader(
-                con.getHeaderField("Content-Encoding").contains("gzip") ? new GZIPInputStream(con.getInputStream())
-                        : con.getInputStream(),
+                contentEncoding.contains("gzip") ? new GZIPInputStream(con.getInputStream()) : con.getInputStream(),
                 "UTF-8"))) {
-            while ((str = urlIn.readLine()) != null) {
-                System.out.println(str);
-            }
+            urlIn.lines().forEach(System.out::println);
         }
         con.disconnect();
     }
