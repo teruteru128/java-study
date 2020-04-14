b7defa8f47e011a25e3ed37bb07d68f8334fa1e0
Teruteru <teruterubouzu1024@gmail.com>
2020-04-14 16:28:16 +0900

    Move ImageLoadTest

---
+++ src/main/java/com/twitter/teruteru128/study/image/ImageLoadTest.java
@@ -0,0 +1,15 @@
+package com.twitter.teruteru128.study.image;
+
+import java.awt.image.ImageProducer;
+
+class ImageLoadTest{
+    public static void main(String[] args) throws Exception{
+        // TODO リポジトリに入れられる画像
+        var resource = ClassLoader.getSystemResource("167887.jpg");
+        var obj = resource.getContent();
+        if (obj instanceof ImageProducer) {
+            ImageProducer producer = (ImageProducer) obj;
+            System.out.println(producer);
+        }
+    }
+}
