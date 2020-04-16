892615e110207979de282b91a9deab283bba92a0
Teruteru <teruterubouzu1024@gmail.com>
2020-04-16 21:14:52 +0900

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@
  */
 module com.twitter.teruteru128.study {
     requires java.desktop;
-//    requires java.json;
+    requires java.json;
     requires java.logging;
     requires java.management;
     requires java.naming;
@@ -27,9 +27,11 @@ module com.twitter.teruteru128.study {
     requires com.h2database;
     requires jakarta.mail;
     requires twitter.text;
+
     exports com.twitter.teruteru128.study.fx.sample1;
     exports com.twitter.teruteru128.study.fx.sample2;
     exports com.twitter.teruteru128.study.fx.sample3;
     exports com.twitter.teruteru128.study.fx.sample4;
-    opens com.twitter.teruteru128.study.jaxb;
+
+    opens com.twitter.teruteru128.study.jaxb to jakarta.xml.bind;
 }
