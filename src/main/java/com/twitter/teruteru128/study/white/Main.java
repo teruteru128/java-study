4ed63ab2dfa5b2ded0f434e4bddbc3ca487450bc
Yuta Onose <onose999999999@gmail.com>
2020-04-16 10:52:28 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/white/Main.java
+++ src/main/java/com/twitter/teruteru128/study/white/Main.java
@@ -1,18 +1,8 @@
 package com.twitter.teruteru128.study.white;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
-
 public class Main {
 
     public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
-        LocalDate endOfDate = LocalDate.of(2020, 7, 24);
-        Period period = today.until(endOfDate);
-        System.out.println(period);
-        System.out.println(period.getDays());
-        System.out.println(today.until(endOfDate, ChronoUnit.DAYS));
     }
 
 }
