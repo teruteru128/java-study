22e2184409fc0ea0065f889991589fc30f7fef66
Teruteru <teruterubouzu1024@gmail.com>
2020-04-19 23:13:45 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,5 +1,9 @@
 package com.twitter.teruteru128.study;
 
+import java.security.MessageDigest;
+
+import jakarta.xml.bind.DatatypeConverter;
+
 /**
  * Main
  * 
@@ -8,5 +12,12 @@ package com.twitter.teruteru128.study;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        byte[] input = new byte[32];
+        byte[] checksum = new byte[32];
+        sha256.update(input, 0, 32);
+        // lenをダイジェスト長以外に出来ない
+        sha256.digest(checksum, 0, 36);
+        System.out.println(DatatypeConverter.printHexBinary(checksum));
     }
 }
