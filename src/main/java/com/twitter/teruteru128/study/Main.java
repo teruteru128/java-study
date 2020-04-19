37b71d79f8da9d26de12bba7afb31943ab4876fb
Teruteru <teruterubouzu1024@gmail.com>
2020-04-19 23:14:12 +0900

    Clean Main

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,9 +1,5 @@
 package com.twitter.teruteru128.study;
 
-import java.security.MessageDigest;
-
-import jakarta.xml.bind.DatatypeConverter;
-
 /**
  * Main
  * 
@@ -12,12 +8,5 @@ import jakarta.xml.bind.DatatypeConverter;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        byte[] input = new byte[32];
-        byte[] checksum = new byte[32];
-        sha256.update(input, 0, 32);
-        // lenをダイジェスト長以外に出来ない
-        sha256.digest(checksum, 0, 36);
-        System.out.println(DatatypeConverter.printHexBinary(checksum));
     }
 }
