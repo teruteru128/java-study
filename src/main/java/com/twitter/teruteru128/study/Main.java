4bcd5a024d7ed4412cd8973427cb4be9dea7fadc
Teruteru <teruterubouzu1024@gmail.com>
2020-04-27 16:09:06 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,5 +1,8 @@
 package com.twitter.teruteru128.study;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Main
  * 
@@ -8,25 +11,7 @@ package com.twitter.teruteru128.study;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // 1回引いて外れる確率
-        System.out.println((double) (1L << 62) - 1);
-        double a = (double) ((1L << 48) - 1) / (1L << 48);
-        double b = 5e-1;
-        double c = 1D / 2D;
-        double min = 0;
-        double max = 1e308;
-        double middle = (min + max) / 2;
-        for (int i = 0; i < 114514; i++) {
-            b = 1 - Math.pow(a, middle);
-            if (b < c) {
-                min = middle;
-            } else if (b > c) {
-                max = middle;
-            }
-            middle = (min + max) / 2;
-        }
-        System.out.printf("%.25g%n", middle);
-        System.out.printf("%.25g%n", Math.pow(a, middle));
-        System.out.printf("%.25g%n", Math.pow(a, middle + 1));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH-mm-ss");
+        System.out.println(formatter.format(LocalDateTime.now()));
     }
 }
