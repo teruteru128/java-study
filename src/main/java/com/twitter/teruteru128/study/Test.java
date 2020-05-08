e3ebc8d65210fecfbff1e3e50ffd7262f6c811b0
Teruteru <teruterubouzu1024@gmail.com>
2020-05-08 10:48:30 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Test.java
+++ src/main/java/com/twitter/teruteru128/study/Test.java
@@ -1,12 +1,14 @@
 package com.twitter.teruteru128.study;
 
-
 public class Test {
 
     public static void main(String[] args) {
-        int i;
-        for (i = 0; i<= 1000;i++){
-            System.out.printf("%d%n",(long) Math.pow(10, (double)(3 * i) / 1000));
+        int x;
+        for (x = 0; x <= 1000; x++) {
+            // x : 1000 - i, y : 0 - (long) Math.pow(10, (double) (3 * i) / 1000))
+            // y = sqrt((1000-x)^2 + 10^(0.006x))
+            double y = Math.pow(10, 3 * x / 1000D);
+            System.out.printf("%d\t%f\t%f%n", x, y, Math.hypot(1000 - x, y));
         }
     }
 }
