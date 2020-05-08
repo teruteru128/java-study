ed2a195cb52d41649dc9eba944686dd44885035b
Teruteru <teruterubouzu1024@gmail.com>
2020-05-08 09:48:52 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Poop.java
+++ src/main/java/com/twitter/teruteru128/study/Poop.java
@@ -1,23 +1,25 @@
 package com.twitter.teruteru128.study;
 
 import java.security.KeyPairGenerator;
-import java.security.spec.NamedParameterSpec;
+import java.security.spec.ECGenParameterSpec;
 
 import javax.crypto.KeyAgreement;
 
 class Poop {
 
     public static void main(String[] args) throws Exception {
+        // U+1F4A9
         String p = "\uD83D\uDCA9";
         System.out.println(p);
-        var generator = KeyPairGenerator.getInstance("XDH");
-        NamedParameterSpec parameterSpec = new NamedParameterSpec("X25519");
+        System.out.printf("%#08x%n", p.codePointAt(0));
+        var generator = KeyPairGenerator.getInstance("EC");
+        ECGenParameterSpec parameterSpec = new ECGenParameterSpec("secp256k1");
         generator.initialize(parameterSpec);
         System.out.println(generator);
         var keyPair = generator.generateKeyPair();
         System.out.println(keyPair.getPrivate());
         System.out.println(keyPair.getPublic());
-        KeyAgreement agreement = KeyAgreement.getInstance("XDH");
+        KeyAgreement agreement = KeyAgreement.getInstance("ECDH");
         agreement.init(keyPair.getPrivate());
     }
 }
