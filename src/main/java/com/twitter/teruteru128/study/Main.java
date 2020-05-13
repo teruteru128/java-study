4194d6e6170d71c367ab2c49ffd9224241f039ac
Teruteru <teruterubouzu1024@gmail.com>
2020-05-13 20:07:35 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,5 +1,7 @@
 package com.twitter.teruteru128.study;
 
+import java.util.Properties;
+
 // TODO 将来的にエントリポイントをこのクラス一つにする
 // TODO コマンドライン引数の実装方法
 // TODO 設定ファイルの実装方法 Propaties XML JSON YAML
@@ -10,20 +12,10 @@ package com.twitter.teruteru128.study;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        byte[] buf = new byte[8192];
-        int len = 0;
-        byte[] b = "0123456789ABCDEF".getBytes();
-        int writeNum = 0;
-        while ((len = System.in.read(buf, 0, 8192)) >= 0) {
-            for (int i = 0; i < len; i++, writeNum++) {
-                System.out.write(b[(buf[i] >>> 4) & 0x0f]);
-                System.out.write(b[(buf[i] >>> 0) & 0x0f]);
-                if ((writeNum % 16) == 15) {
-                    System.out.println();
-                }
-            }
-        }
-        // 後始末
-        System.out.flush();
+        Thread poop = new Thread(new Poop());
+        poop.start();
+        Properties properties = new Properties();
+        properties.setProperty("\uD83D\uDCA9", "やったぜ。");
+        properties.store(System.out, null);
     }
 }
