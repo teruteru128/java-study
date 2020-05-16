087a2569adeb10b23dddc39618dd368422714ea8
Teruteru <teruterubouzu1024@gmail.com>
2020-05-16 17:09:20 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,5 +1,7 @@
 package com.twitter.teruteru128.study;
 
+import java.io.InputStream;
+import java.util.Properties;
 
 // TODO 将来的にエントリポイントをこのクラス一つにする
 // TODO コマンドライン引数の実装方法
@@ -14,7 +16,11 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("${project.version}");
+        Properties properties = new Properties();
+        try(InputStream in = Main.class.getResourceAsStream("config.properties")){
+            properties.load(in);
+        }
+        System.out.printf("%s%n", properties.getProperty("version"));
         System.out.println("この始末☆");
     }
 }
