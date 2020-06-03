20db137d12b7b8aa6026a1c1f8ffc05e3691a650
Teruteru <teruterubouzu1024@gmail.com>
2020-06-04 08:58:26 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -10,25 +10,10 @@ package com.twitter.teruteru128.study;
 public class Main {
 
     /**
-     * U+0028
-     * U+0020
-     * U+ff9f
-     * U+0434
-     * U+ff9f
-     * U+0029
-     * U+ff64
-     * U+ff8d
-     * U+ff9f
-     * U+ff6f
      * 
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        String pe = "( ﾟдﾟ)､ﾍﾟｯ";
-        int len = pe.length();
-        for(int i = 0; i < len; i++){
-            System.out.printf("U+%04x\n", pe.codePointAt(i));
-        }
     }
 }
