1400d574248adca3eb616d80c9b61054308f6f72
Teruteru <teruterubouzu1024@gmail.com>
2020-06-05 02:24:01 +0900

    Update

--- src/test/java/com/twitter/teruteru128/ColorCodeTest.java
+++ src/test/java/com/twitter/teruteru128/ColorCodeTest.java
@@ -6,6 +6,6 @@ public class ColorCodeTest{
 
   @Test
   public void colorCodeTest(){
-    System.out.println("ひぐらしの\u001b[31mな\u001b[0mく頃に");
+    System.err.println("ひぐらしの\u001b[31mな\u001b[0mく頃に");
   }
 }
