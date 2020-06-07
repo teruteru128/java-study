625773df42277a17b9e31b877d9a09d2b3670e13
Teruteru <teruterubouzu1024@gmail.com>
2020-06-08 00:14:01 +0900

    Clean Main

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,12 +1,5 @@
 package com.twitter.teruteru128.study;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
-// TODO 将来的にエントリポイントをこのクラス一つにする
-// TODO コマンドライン引数の実装方法
-// TODO 設定ファイルの実装方法 Propaties XML JSON YAML
 /**
  * Main
  * 
@@ -14,13 +7,10 @@ import java.util.Scanner;
 public class Main {
 
     /**
-     * thread1->pubkeys0.bin thread2->pubkeys1.bin thread3->pubkeys2.bin
-     * thread4->pubkeys3.bin
      * 
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
