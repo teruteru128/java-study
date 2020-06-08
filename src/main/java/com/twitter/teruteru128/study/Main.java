d40471dcc51a7ea3e5e4c67ec16134d451da6929
Teruteru <teruterubouzu1024@gmail.com>
2020-06-08 11:01:49 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,5 +1,7 @@
 package com.twitter.teruteru128.study;
 
+import java.util.concurrent.ForkJoinPool;
+
 /**
  * Main
  * 
@@ -12,5 +14,17 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+        int order = 14;
+        long x = 64000;
+        long y = 32000;
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        System.out.printf("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>%n");
+        System.out.printf("<svg xmlns=\"http://www.w3.org/2000/svg\" ");
+        System.out.printf("version=\"1.1\" width=\"%d\" height=\"%d\">%n", x, y);
+        System.out.printf("<path d=\"");
+        forkJoinPool.invoke(new DragonAction(order, 1L<<7, 0, 1));
+        System.out.printf("Z");
+        System.out.printf("\" fill=\"none\" stroke=\"black\" />%n");
+        System.out.printf("</svg>%n");
     }
 }
