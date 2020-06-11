5e75c2c2452b3d67ba7482b1e7c29f14ab5d96df
Teruteru <teruterubouzu1024@gmail.com>
2020-06-11 11:06:10 +0900

    Update Nonlinear Adjacent Trinomial Recurrence Formula

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,6 +1,8 @@
 package com.twitter.teruteru128.study;
 
-import com.twitter.teruteru128.study.tcp.client.Client;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
  * Main
@@ -14,7 +16,23 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        Thread thread = new Thread(new Client());
-        thread.start();
+        final MathContext context = new MathContext(52, RoundingMode.HALF_EVEN);
+        final BigDecimal three = BigDecimal.valueOf(3);
+        final BigDecimal threshold = BigDecimal.valueOf(10000);
+        final BigDecimal a0 = BigDecimal.ONE;
+        final BigDecimal a1 = new BigDecimal("1.5788573135586235");
+        BigDecimal a = BigDecimal.ZERO;
+        BigDecimal an2 = a0;
+        BigDecimal an = a1;
+        for (int i = 0; i < 100; i++) {
+            a = an2.pow(3).add(an).divide(three, context);
+            if (a.compareTo(threshold) >= 0) {
+                System.out.println("over!");
+                break;
+            }
+            an2 = an;
+            an = a;
+            System.out.println(a);
+        }
     }
 }
