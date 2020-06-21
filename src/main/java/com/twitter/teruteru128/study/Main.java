960613572949862f63185d8c87e23bc2f22970ea
Teruteru <teruterubouzu1024@gmail.com>
2020-06-22 01:59:51 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -12,7 +12,12 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        String a = "";
-        System.out.println(a.hashCode());
+        long newseed = 0;
+        long minseed = 1L << 48;
+        for (long seed = 0x0; seed < 0x10000; seed++) {
+            newseed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
+            minseed = Math.min(newseed, minseed);
+        }
+        System.out.println(minseed);
     }
 }
