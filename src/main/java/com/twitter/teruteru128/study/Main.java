644045dd4d058b28f7cb75ab880ac4f7559d65c1
Teruteru <teruterubouzu1024@gmail.com>
2020-06-22 13:10:05 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -8,28 +8,31 @@ import java.util.Random;
  */
 public class Main {
 
-    static boolean isSlimeChunk(Random random, long seed, int x, int z) {
-        random.setSeed(seed + memoX[x + 625] + memoZ[z + 625]);
+    private static boolean isSlimeChunk(Random random, long seed, int x, int z) {
+        random.setSeed((seed + memoX[x + 625] + memoZ[z + 625]) ^ 0x3ad8025f);
+        //random.setSeed(seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int) (z * z) * 0x4307a7L + (int) (z * 0x5f24f) ^ 0x3ad8025f);
         return random.nextInt(10) == 0;
     }
 
-    private static final int[] memoX = new int[1250];
-    private static final int[] memoZ = new int[1250];
+    private static final long[] memoX = new long[1250];
+    private static final long[] memoZ = new long[1250];
 
     static {
-        for (int i = 0, offsetI, i2; i < 1250; i++) {
+        int offsetI, i2;
+        for (int i = 0; i < 1250; i++) {
             offsetI = i - 625;
             i2 = offsetI * offsetI;
-            memoX[i] = i2 * 0x4c1906 + offsetI * 0x5ac0db;
-            memoZ[i] = (int) (i2 * 0x4307a7L) + (offsetI * 0x5f24f) ^ 0x3ad8025f;
+            memoX[i] = i2 * 0x4c1906;
+            memoX[i] += offsetI * 0x5ac0db;
+            memoZ[i] = i2 * 0x4307a7L + offsetI * 0x5f24f;
         }
     }
 
     /**
      * @param random
      * @param seed
-     * @param x
-     * @param z
+     * @param x 湧き層の北西チャンクのxチャンク座標
+     * @param z 湧き層の北西チャンクのzチャンク座標
      * @param sign -1 -> north, 1 -> south
      */
     private static void searchBranch(Random random, long seed, int x, int z, int sign) {
@@ -82,6 +85,12 @@ public class Main {
         random.setSeed(74803317123181L);
         System.out.printf("74803317123181: %f%n", random.nextDouble());
         System.out.printf("74803317123181: %f%n", random.nextDouble());
+        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+0));
+        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+1));
+        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+2));
+        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+3));
+        System.out.println(isSlimeChunk(random, 127287216738988L, -7264/16, -9680/16));
+        System.out.printf("%x016x%n", memoX[-196 + 625]);
 
         for (long i = 0x0000_0000_0000L; i < 0x0000_0001_0000L; i++) {
             seed = 0x0000_0000_0000L + i;
@@ -103,21 +112,26 @@ public class Main {
         int world_cursol_z;
         int count = 0;
         // ブランチマイニング
-        /*
-        for (world_cursol_z = -625; world_cursol_z < 625; world_cursol_z += 5) {
-            for (world_cursol_x = -625; world_cursol_x < 625; world_cursol_x++) {
-
-                if (isSlimeChunk(random, seed, world_cursol_x, world_cursol_z)) {
-                    count++;
-                    if (count == 5) {
-                        searchBranch(random, seed, world_cursol_x, world_cursol_z, 1);
-                        searchBranch(random, seed, world_cursol_x, world_cursol_z, -1);
+        seed = random.nextLong() & 0xffffffffffffL;
+        long max = seed - 1;
+        for(; seed != max; seed = (seed + 1) & 0xffffffffffffL)
+        {
+            for (world_cursol_z = -625; world_cursol_z < 625; world_cursol_z += 5) {
+                count = 0;
+                for (world_cursol_x = -625; world_cursol_x < 625; world_cursol_x++) {
+                    
+                    if (isSlimeChunk(random, seed, world_cursol_x, world_cursol_z)) {
+                        count++;
+                        if (count >= 5) {
+                            searchBranch(random, seed, world_cursol_x, world_cursol_z, 1);
+                            searchBranch(random, seed, world_cursol_x, world_cursol_z, -1);
+                            System.out.printf("yay!!!%d, %d, %d, %d%n", count, seed, world_cursol_x*16, world_cursol_z*16);
+                        }
+                    } else {
+                        count = 0;
                     }
-                } else {
-                    count = 0;
                 }
             }
         }
-        */
     }
 }
