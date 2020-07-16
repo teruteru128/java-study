3464d269c2f337b52fdf318782b913f5399b6aeb
Teruteru <teruterubouzu1024@gmail.com>
2020-07-16 13:03:40 +0900

    Update

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,15 +1,19 @@
 package com.twitter.teruteru128.study;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.Random;
 
 /**
- * Main
- * Slime miner(作りかけ)
+ * Main Slime miner(作りかけ)
  */
 public class Main {
 
     private static long s(long seed, int x, int z) {
-        return seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int) (z * z) * 0x4307a7L + (int) (z * 0x5f24f) ^ 0x3ad8025f;
+        return seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int) (z * z) * 0x4307a7L + (int) (z * 0x5f24f)
+                ^ 0x3ad8025f;
     }
 
     private static long s1(long seed, int x, int z) {
@@ -18,7 +22,8 @@ public class Main {
 
     private static boolean isSlimeChunk(Random random, long seed, int x, int z) {
         random.setSeed(s1(seed, x, z));
-        //random.setSeed(seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int) (z * z) * 0x4307a7L + (int) (z * 0x5f24f) ^ 0x3ad8025f);
+        // random.setSeed(seed + (int) (x * x * 0x4c1906) + (int) (x * 0x5ac0db) + (int)
+        // (z * z) * 0x4307a7L + (int) (z * 0x5f24f) ^ 0x3ad8025f);
         return random.nextInt(10) == 0;
     }
 
@@ -39,9 +44,9 @@ public class Main {
     /**
      * @param random
      * @param seed
-     * @param x 湧き層の北西チャンクのxチャンク座標
-     * @param z 湧き層の北西チャンクのzチャンク座標
-     * @param sign -1 -> north, 1 -> south
+     * @param x      湧き層の北西チャンクのxチャンク座標
+     * @param z      湧き層の北西チャンクのzチャンク座標
+     * @param sign   -1 -> north, 1 -> south
      */
     private static void searchBranch(Random random, long seed, int x, int z, int sign) {
         int spawn_floor_x;
@@ -60,113 +65,15 @@ public class Main {
         int x, z;
         long prevseed1 = 0;
         long prevseed2 = 0;
-        for(seed = 0; seed  < 1; seed++) {
-            for(z = -625; z  < 625; z++) {
-                for(x = -625; x < 625; x++) {
-                    if(s(seed, x, z) != s1(seed, x, z))
-                    System.out.printf("false : %d, %d\n", x, z);
+        Path out = Paths.get("out.txt");
+        LinkedList<String> list = new LinkedList<>();
+        for (seed = 0; seed < 1; seed++) {
+            for (z = -625; z < 625; z++) {
+                for (x = -625; x < 625; x++) {
+                    list.add(Long.toString(s(seed, x, z)));
                 }
             }
         }
-        /*
-        seed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL;
-        System.out.printf("%d%n", seed);
-        seed = 246345201500483L ^ 0x5DEECE66DL;
-        seed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL;
-        System.out.printf("%012x%n", seed);
-        seed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL;
-        System.out.printf("%012x%n", seed);
-        seed = 0;
-        seed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-        System.out.printf("%d%n", seed);
-        System.out.printf("%d%n", seed ^ 0x5DEECE66DL);
-        seed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-        seed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-        System.out.printf("%d%n", seed ^ 0x5DEECE66DL);
-        seed = 0x4498;
-        seed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-        seed = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-        System.out.printf("0x4498 -> %d%n", seed);
-        System.out.printf("0x4498 -> %d%n", seed ^ 0x5DEECE66DL);
-        System.out.printf("0x4498 -> %012x%n", seed);
-        System.out.printf("0x4498 -> %012x%n", seed & 0x0000ffff0000L);
-        System.out.printf("0x4498 -> %012x%n", seed ^ 0x5DEECE66DL);
-        Random random = new Random();
-        random.setSeed(246345201500483L);
-        System.out.printf("246345201500483 : %x%n", random.nextLong());
-        System.out.printf("246345201500483 : %x%n", random.nextLong());
-        random.setSeed(164311266871034L);
-        System.out.printf("164311266871034 : %a%n", random.nextDouble());
-        System.out.printf("164311266871034 : %a%n", random.nextDouble());
-        random.setSeed(74803317123181L);
-        System.out.printf("74803317123181: %012x%n", random.nextLong());
-        System.out.printf("74803317123181: %012x%n", random.nextLong());
-        random.setSeed(74803317123181L);
-        System.out.printf("74803317123181: %f%n", random.nextDouble());
-        System.out.printf("74803317123181: %f%n", random.nextDouble());
-        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+0));
-        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+1));
-        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+2));
-        System.out.println(isSlimeChunk(random, 1613738097659009556L, -196+0, -150+3));
-        System.out.printf("%x016x%n", memoX[-196 + 625]);
-        */
-        /*
-        for (long i = 0x0000_0000_0000L; i < 0x0000_0001_0000L; i++) {
-            seed = 0xffff_ffff_0000L + i;
-            prevseed1 = (seed - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-            if ((prevseed1 & 0x0000_ffff_0000L) == 0x0000_0000_0000L) {
-                prevseed2 = (prevseed1 - 0xBL) * 0xDFE05BCB1365L & 0xFFFFFFFFFFFFL;
-                System.out.printf("x : %012x%n", seed);
-                System.out.printf("x : %012x%n", prevseed1);
-                System.out.printf("x : %012x%n", prevseed2);
-                System.out.printf("x : %012x%n", prevseed2 ^ 0x5DEECE66DL);
-                System.out.printf("d : %d%n", prevseed2 ^ 0x5DEECE66DL);
-                random.setSeed(prevseed2 ^ 0x5DEECE66DL);
-                System.out.printf("x : %012x%n", random.nextLong());
-                System.out.printf("x : %012x%n", random.nextLong());
-                random.setSeed(prevseed2 ^ 0x5DEECE66DL);
-                System.out.printf("x : %08x%n", random.nextInt());
-                System.out.printf("x : %08x%n", random.nextInt());
-                random.setSeed(prevseed2 ^ 0x5DEECE66DL);
-                System.out.printf("a : %a%n", random.nextDouble());
-                System.out.printf("a : %a%n", random.nextDouble());
-                System.out.println("--");
-            }
-        }
-        */
-        /** -626 ~ 625 */
-        int world_cursol_x;
-        /** -626 ~ 625 */
-        int world_cursol_z;
-        int count = 0;
-        // ブランチマイニング
-        /*
-        seed = random.nextLong() & 0xffffffffffffL;
-        long max = seed - 1;
-        for(; seed != max; seed = (seed + 1) & 0xffffffffffffL)
-        {
-            for (world_cursol_z = -625; world_cursol_z < 625; world_cursol_z += 5) {
-                count = 0;
-                for (world_cursol_x = -625; world_cursol_x < 625; world_cursol_x++) {
-                    
-                    if (isSlimeChunk(random, seed, world_cursol_x, world_cursol_z)) {
-                        count++;
-                        if (count >= 5) {
-                            searchBranch(random, seed, world_cursol_x, world_cursol_z, 1);
-                            searchBranch(random, seed, world_cursol_x, world_cursol_z, -1);
-                            System.out.printf("yay!!!%d, %d, %d, %d%n", count, seed, world_cursol_x*16, world_cursol_z*16);
-                        }
-                    } else {
-                        count = 0;
-                    }
-                }
-            }
-        }
-        long head = 0;
-        long tail = 164311266871034L;
-        for (; head < 21; head++) {
-            System.out.println((head << 48) + tail);
-        }
-        */
+        Files.write(out, list);
     }
 }
---
+++ src/main/java/com/twitter/teruteru128/study/bitmessage/genaddress/RipeAndKeysConverter.java
@@ -0,0 +1,67 @@
+package com.twitter.teruteru128.study.bitmessage.genaddress;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.regex.Pattern;
+import com.twitter.teruteru128.study.bitmessage.Const;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import jakarta.xml.bind.DatatypeConverter;
+
+public class RipeAndKeysConverter {
+
+    private static byte[] derivePublicKey(byte[] b) {
+        return Const.G.multiply(new BigInteger(1, b)).normalize().getEncoded(false);
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, DigestException {
+        Pattern pattern = Pattern.compile(",");
+        Provider provider = Security.getProvider("BC");
+        if (provider == null) {
+            Security.addProvider(provider = new BouncyCastleProvider());
+        }
+        String[] a = null;
+        byte[] privatesigningkey;
+        byte[] privateencryptkey;
+        byte[] publicsigningkey;
+        byte[] publicencryptkey;
+        final MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+        final MessageDigest ripemd160 = MessageDigest.getInstance("RIPEMD160");
+        final byte[] cache64 = new byte[Const.SHA512_DIGEST_LENGTH];
+        final byte[] ripe = new byte[Const.RIPEMD160_DIGEST_LENGTH];
+        String address4;
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(ClassLoader.getSystemResourceAsStream("ripeandkeys.txt")));
+                PrintStream ps = new PrintStream(new File("keys.txt"))) {
+            String header = reader.readLine();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                a = pattern.split(line);
+                privatesigningkey = DatatypeConverter.parseHexBinary(a[1]);
+                privateencryptkey = DatatypeConverter.parseHexBinary(a[2]);
+                publicsigningkey = derivePublicKey(privatesigningkey);
+                publicencryptkey = derivePublicKey(privateencryptkey);
+                sha512.update(publicsigningkey, 0, Const.PUBLIC_KEY_LENGTH);
+                sha512.update(publicencryptkey, 0, Const.PUBLIC_KEY_LENGTH);
+                sha512.digest(cache64, 0, Const.SHA512_DIGEST_LENGTH);
+                ripemd160.update(cache64, 0, Const.SHA512_DIGEST_LENGTH);
+                ripemd160.digest(ripe, 0, Const.RIPEMD160_DIGEST_LENGTH);
+                Response response = new Response(new KeyPair(privatesigningkey, publicsigningkey),
+                        new KeyPair(privateencryptkey, privateencryptkey), ripe);
+                BMAddressGenerator.exportAddressTo(response, ps);
+            }
+        }
+    }
+}
