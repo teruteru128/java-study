c4b20439c9c79a3514f17d12e8ed0e1c24d995a5
Teruteru <teruterubouzu1024@gmail.com>
2021-05-01 01:13:52 +0900

    Clean Main

--- src/main/java/com/twitter/teruteru128/study/Main.java
+++ src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,19 +1,9 @@
 package com.twitter.teruteru128.study;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.nio.file.Files;
-
 /**
  * Main
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        var file = new File("/home/teruteru128/git/study/src/262144bit-initialValue2.txt");
-        var lines = Files.readAllLines(file.toPath());
-        var base = new BigInteger(lines.get(0), 16).add(BigInteger.valueOf(52269));
-        System.out.println(base.bitLength());
-        System.out.println(base.isProbablePrime(1));
+    public static void main(String[] args) {
     }
 }
