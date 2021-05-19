094d1268f7c069a6ae03735a70d02737eb5897b5
Teruteru <teruterubouzu1024@gmail.com>
2021-05-19 18:21:05 +0900

    Rename

--- src/test/java/com/twitter/teruteru128/ClassCallingFromModuleTest.java
+++ src/test/java/com/twitter/teruteru128/ClassCallingFromModuleTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class DatatypeConverterTest {
+public class ClassCallingFromModuleTest {
 
     private static String MODULE_NAME = "jakarta.xml.bind";
 
