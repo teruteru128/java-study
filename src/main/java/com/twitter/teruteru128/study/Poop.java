5968442dff2872475e8df5d6d45ccc2c871e4ca4
Teruteru <teruterubouzu1024@gmail.com>
2021-07-01 18:45:27 +0900

    Add surrogate pair sample

--- src/main/java/com/twitter/teruteru128/study/Poop.java
+++ src/main/java/com/twitter/teruteru128/study/Poop.java
@@ -9,14 +9,18 @@ public class Poop implements Runnable {
     public void run() {
         // U+1F4A9
         var p = "\uD83D\uDCA9";
+        var q = Character.toChars(0x1f4a9);
         System.out.println(p);
         System.out.printf("U+%04X%n", p.codePointAt(0));
         System.out.printf("\\u%04X\\u%04X%n", (int) p.charAt(0), (int) p.charAt(1));
+        System.out.printf("\\u%04X\\u%04X%n", (int) q[0], (int) q[1]);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         var poop = new Thread(new Poop());
         poop.start();
+        poop.join();
+        System.out.println("--");
         var properties = new Properties(System.getProperties());
         properties.setProperty("\uD83D\uDCA9", "やったぜ。");
         properties.store(System.out, null);
