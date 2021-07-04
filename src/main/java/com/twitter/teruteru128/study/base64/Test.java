0e0a25533dd06d758b9080b456ff23dfa0a2e372
Teruteru <teruterubouzu1024@gmail.com>
2021-07-04 19:30:34 +0900

    Refactoring

--- src/main/java/com/twitter/teruteru128/study/base64/Test.java
+++ src/main/java/com/twitter/teruteru128/study/base64/Test.java
@@ -2,7 +2,7 @@ package com.twitter.teruteru128.study.base64;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 /**
@@ -15,12 +15,13 @@ public class Test {
      * @param args
      */
     public static void main(String[] args) {
-        byte[] buffer = new byte[2048];
-        Base64.Encoder encoder = Base64.getMimeEncoder(76, System.lineSeparator().getBytes(Charset.forName("UTF-8")));
-        int readed_length;
-        try (BufferedInputStream bin = new BufferedInputStream(System.in);BufferedOutputStream bout = new BufferedOutputStream(encoder.wrap(System.out))) {
-            while((readed_length = bin.read(buffer,0,buffer.length)) != -1){
-                bout.write(buffer, 0, readed_length);
+        var buffer = new byte[2048];
+        var encoder = Base64.getMimeEncoder(76, System.lineSeparator().getBytes(StandardCharsets.UTF_8));
+        int length;
+        try (var bin = new BufferedInputStream(System.in);
+                var bout = new BufferedOutputStream(encoder.wrap(System.out))) {
+            while ((length = bin.read(buffer, 0, buffer.length)) != -1) {
+                bout.write(buffer, 0, length);
             }
         } catch (Exception e) {
             e.printStackTrace();
