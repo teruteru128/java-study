6f3dfb12e4c139506492e64aeadb94f3375230bc
Teruteru <teruterubouzu1024@gmail.com>
2021-11-20 17:38:45 +0900

    Update

---
+++ fx/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.twitter.teruteru.fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires org.twitter4j.core;
+}
---
+++ fxapp/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.twitter.teruteru.fxapp {
+    requires javafx.fxml;
+    requires javafx.graphics;
+}
