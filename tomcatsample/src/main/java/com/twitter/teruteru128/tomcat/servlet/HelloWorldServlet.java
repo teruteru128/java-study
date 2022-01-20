e576098f4e350961d8c0705c17d2d0196af2d198
Teruteru <teruterubouzu1024@gmail.com>
2022-01-20 22:15:12 +0900

    Add embedded Tomcat sample

---
+++ tomcatsample/src/main/java/com/twitter/teruteru128/tomcat/servlet/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package com.twitter.teruteru128.tomcat.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MyServlet", urlPatterns = { "/hello" })
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try (ServletOutputStream out = resp.getOutputStream()) {
+            out.write("hello world!".getBytes());
+        }
+    }
+
+}
