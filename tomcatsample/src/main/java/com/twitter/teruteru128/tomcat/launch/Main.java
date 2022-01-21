4307148ea037d23ebfd7d9792276b1c6b1b8cc80
Teruteru <teruterubouzu1024@gmail.com>
2022-01-21 22:56:18 +0900

    なにもわからない

--- tomcatsample/src/main/java/com/twitter/teruteru128/tomcat/launch/Main.java
+++ tomcatsample/src/main/java/com/twitter/teruteru128/tomcat/launch/Main.java
@@ -1,38 +1,58 @@
 package com.twitter.teruteru128.tomcat.launch;
 
 import java.io.File;
-
-import com.twitter.teruteru128.tomcat.servlet.HelloWorldServlet;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.ContextConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+import org.apache.coyote.http11.Http11NioProtocol;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String webappDirLocation = "src/main/webapp/";
-        Tomcat tomcat = new Tomcat();// Add role and user order is important
+        Tomcat tomcat = new Tomcat();
+        var protocol = Http11NioProtocol.class.getName();
+        var connector = new Connector(protocol);
+        connector.setPort(0);
+        connector.setThrowOnFailure(true);
+        var service = tomcat.getService();
+        service.addConnector(connector);
+        tomcat.setConnector(connector);
+        tomcat.setBaseDir(null);
+        File tempBase = new File(System.getProperty("tomcat.test.temp", "output/tmp"));
+        if (!tempBase.mkdirs() && !tempBase.isDirectory()){
+            return;
+        }
+        Path tempBasePath = FileSystems.getDefault().getPath(tempBase.getAbsolutePath());
+        File tempDir = Files.createTempDirectory(tempBasePath, "test").toFile();
+        File appBase = new File(tempDir, "webapps");
+        tomcat.getHost().setAppBase(appBase.getAbsolutePath());
         // The port that we should run on can be set into an environment variable
         // Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
-        tomcat.setPort(Integer.valueOf(webPort));
-        tomcat.getConnector();
+        var ctx = (StandardContext) tomcat.addContext("", null);
+        // ctx.addLifecycleListener(tomcat.getDefaultWebXmlListener());
+        var config = new ContextConfig();
+        config.setDefaultWebXml(tomcat.noDefaultWebXmlPath());
+        ctx.addLifecycleListener(config);
+/* 
+        ctx.setResources(new StandardRoot(ctx));
+        var currentPath = new File("./build/classes/java/main").getAbsolutePath();
+        ctx.getResources().createWebResourceSet(WebResourceRoot.ResourceSetType.POST, "/WEB-INF/classes", currentPath, null, "/");
+        ctx.setAddWebinfClassesResources(true); */
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("",
-                new File(webappDirLocation).getAbsolutePath());
-        // declare an alternate location for your "WEB-INF/classes" dir
-        // Servlet 3.0 annotation will work
-        File additionWebInfClasses = new File("build/classes/java/main");
-        WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-                additionWebInfClasses.getAbsolutePath(), "/"));
-        ctx.setResources(resources);
+        // TODO 手動で読み込ませずに、アノテーションでサーブレットを読ませるにはどうしたらいいんだ？
+        //Tomcat.addServlet(ctx, "MyServlet", new HelloWorldServlet());
+        //ctx.addServletMappingDecoded("/hello", "MyServlet");
+
         tomcat.start();
+        System.out.printf("http://localhost:%d/hello", connector.getLocalPort());
         tomcat.getServer().await();
     }
 }
