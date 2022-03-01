9abf5140ebbc25d2e81b8503b03a0e54d856db0d
Teruteru <teruterubouzu1024@gmail.com>
2022-03-02 01:34:29 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,8 +1,7 @@
 package com.twitter.teruteru128.study;
 
-import java.math.BigInteger;
-
-import com.twitter.teruteru128.study.collatz.Collatz5;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Main
@@ -10,7 +9,14 @@ import com.twitter.teruteru128.study.collatz.Collatz5;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Collatz5 c = new Collatz5();
-        c.accept(BigInteger.valueOf(3));
+        /* Collatz5 c = new Collatz5(); */
+        /* c.accept(BigInteger.valueOf(3)); */
+        // シャンフロ「2ヶ月以上更新されていません」の注意書きが出るまでの期間
+        var d2 = LocalDateTime.of(2022, 4, 4, 14, 4, 0);
+        var now = LocalDateTime.now();
+        for (var unit : ChronoUnit.values()) {
+            if (now.isSupported(unit))
+                System.out.printf("%s, %s%n", unit, now.until(d2, unit));
+        }
     }
 }
