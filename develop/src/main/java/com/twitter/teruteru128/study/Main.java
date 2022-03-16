999f824b7a4d64cbc828dd8f47cded3472207615
Teruteru <teruterubouzu1024@gmail.com>
2022-03-16 22:40:22 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,24 +1,53 @@
 package com.twitter.teruteru128.study;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
 import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.twitter.teruteru128.study.bitmessage.AddressKeyPairs;
 import com.twitter.teruteru128.study.bitmessage.KeyPair;
-import com.twitter.teruteru128.study.bitmessage.MultiThreadRipeGenerator;
 
 /**
  * Main
  */
 public class Main {
+
+    private static final class CallableImplementation implements Callable<List<KeyPair>> {
+        private int i;
+
+        public CallableImplementation(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public List<KeyPair> call() throws Exception {
+            var list = new ArrayList<KeyPair>(16777216 * 8);
+            byte[] pribuf = new byte[32];
+            byte[] pubbuf = new byte[65];
+            byte[] pubkeysallbyte = Files.readAllBytes(new File(String.format("../publicKeys%d.bin", i)).toPath());
+            byte[] prikeysallbyte = Files.readAllBytes(new File(String.format("../privateKeys%d.bin", i)).toPath());
+            int pubkeyoffset = 0;
+            int prikeyoffset = 0;
+            for (var j = 0; j < 16777216; j++, pubkeyoffset += 65, prikeyoffset += 32) {
+                System.arraycopy(pubkeysallbyte, pubkeyoffset, pubbuf, 0, 65);
+                System.arraycopy(prikeysallbyte, prikeyoffset, pribuf, 0, 32);
+                list.add(new KeyPair(pribuf, pubbuf));
+            }
+            Runtime.getRuntime().gc();
+            return list;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         var service = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(12);
@@ -34,35 +63,34 @@ public class Main {
                     .append((char) random.nextInt('A', 'Z' + 1))
                     .append((char) random.nextInt('A', 'Z' + 1)).append((char) random.nextInt('A', 'Z' + 1)).toString();
         }, 0, TimeUnit.NANOSECONDS);
-        var keysFuture = service.schedule(() -> {
-            var list = new ArrayList<KeyPair>(16777216 * 8);
-            byte[] pribuf = new byte[32];
-            byte[] pubbuf = new byte[65];
-            for (int i = 0; i < 8; i++) {
-                try (RandomAccessFile pubfile = new RandomAccessFile(new File(String.format("publicKeys%d.bin", i)),
-                        "r");
-                        FileChannel pubchannel = pubfile.getChannel();
-                        RandomAccessFile prifile = new RandomAccessFile(new File(String.format("privateKeys%d.bin", i)),
-                                "r");
-                        FileChannel prichannel = pubfile.getChannel()) {
-                    MappedByteBuffer pubbuffer = pubchannel.map(MapMode.READ_ONLY, 0, pubfile.length());
-                    MappedByteBuffer pribuffer = prichannel.map(MapMode.READ_ONLY, 0, prifile.length());
-                    pubbuffer.get(pubbuf);
-                    pribuffer.get(pribuf);
-                    list.add(new KeyPair(pribuf, pubbuf));
-                }
-            }
-            return list;
-        }, 0, TimeUnit.NANOSECONDS);
-        var keyPairList = keysFuture.get();
-        // 2の27乗件(1億3000万件)のsecp256k1鍵があるので70兆件ぐらいのアドレスがフィルターを通過するはず
-        keyPairList.parallelStream().flatMap(s -> keyPairList.stream().map(e -> new AddressKeyPairs(s, e)))
-                .map(new MultiThreadRipeGenerator()).filter(a -> a.getRipe()[0] == 0);
-        // service.schedule(new UTF8DecodeSample(), 0, TimeUnit.NANOSECONDS);
         System.out.println(randomFuture.get());
+        var start = LocalDateTime.now();
+        List<Callable<List<KeyPair>>> cList = new ArrayList<>();
+        for (int k = 0; k < 8; k++) {
+            cList.add(new CallableImplementation(k));
+        }
+        System.out.println("p");
+        var keysFuture = service.invokeAll(cList);
+        System.out.println("q");
+        var keyPairList = new ArrayList<KeyPair>();
         service.schedule(() -> {
             System.out.println("シャットダウンします……");
             service.shutdown();
         }, 0, TimeUnit.SECONDS);
+        for (Future<List<KeyPair>> f : keysFuture) {
+            keyPairList.addAll(f.get());
+        }
+        var finish = LocalDateTime.now();
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File("/mnt/g/Downloads/ooskeypairlist.bin")), 1024*1024*1024))) {
+            oos.writeObject(keyPairList);
+        }
+        System.out.printf("%d, %s%n", keyPairList.size(), start.until(finish, ChronoUnit.MILLIS));
+        // 2の27乗件(1億3000万件)のsecp256k1鍵があるので1京8000兆件ぐらいのアドレスが生成されて70兆件ぐらいのアドレスがフィルターを通過するはず
+        /*
+         * keyPairList.parallelStream().flatMap(s -> keyPairList.stream().map(e -> new
+         * AddressKeyPairs(s, e)))
+         * .map(new MultiThreadRipeGenerator()).filter(a -> a.getRipe()[0] == 0);
+         */
+        // service.schedule(new UTF8DecodeSample(), 0, TimeUnit.NANOSECONDS);
     }
 }
