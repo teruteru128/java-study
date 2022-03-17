b5610e99d4f609423c60005324370317413551a5
Teruteru <teruterubouzu1024@gmail.com>
2022-03-18 02:56:41 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,11 +1,7 @@
 package com.twitter.teruteru128.study;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -14,7 +10,6 @@ import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
@@ -23,8 +18,6 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.IntFunction;
 
-import com.google.common.base.Supplier;
-import com.twitter.teruteru128.study.sf.Shangri_laFrontierCountDown;
 import com.twitter.teruteru128.study.utf8.UTF8DecodeSample;
 
 /**
@@ -35,11 +28,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         var service = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(12);
-        service.schedule(new Shangri_laFrontierCountDown(), 0, TimeUnit.NANOSECONDS);
-        // service.schedule(new ServiceCallSample(), 0, TimeUnit.NANOSECONDS);
-        // var study = args.length >= 1 ? new SwingStudy(Path.of(args[0])) : new
-        // SwingStudy();
-        // service.schedule(study, 0, TimeUnit.NANOSECONDS);
         // 普通こうやって時間のかかる処理を裏でやらせるための機能だよね……？
         var start = LocalDateTime.now();
         var randomFuture = service.schedule(() -> {
@@ -54,6 +42,7 @@ public class Main {
         service.schedule(new UTF8DecodeSample(), 0, TimeUnit.NANOSECONDS);
         List<Callable<String>> list = new ArrayList<>(16);
         // 1つのint値引数を受け取って「結果を返し、例外をスローすることがあるシリアライズ可能なタスク」を生成する関数
+        @SuppressWarnings("unchecked")
         var intfunction = (IntFunction<Callable<String>>) i -> (Callable<String> & Serializable) () -> Integer.toString(i);
         // Listにタスクを詰める
         for (int i = 0; i < 16; i++) {
