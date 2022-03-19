b31dc9883c1916e220b8d183a2b4460fa188f571
Teruteru <teruterubouzu1024@gmail.com>
2022-03-19 20:41:02 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,11 +1,10 @@
 package com.twitter.teruteru128.study;
 
-import java.nio.file.Path;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import com.twitter.teruteru128.study.file.Storage;
+import com.twitter.teruteru128.study.onion.DeepWebRadioPoller;
 
 /**
  * Main
@@ -14,14 +13,12 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         var service = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(12);
-        var future = service.schedule(new Storage(), 0, TimeUnit.NANOSECONDS);
+        var future = service.schedule(new DeepWebRadioPoller(), 0, TimeUnit.NANOSECONDS);
         var shutdownFuture = service.schedule(() -> {
             System.out.println("シャットダウンします……");
             service.shutdown();
         }, 500, TimeUnit.MILLISECONDS);
-        for (Path path : future.get()) {
-            System.out.println(path);
-        }
+        future.get();
         shutdownFuture.get();
     }
 }
--- develop/src/main/java/com/twitter/teruteru128/study/onion/DeepWebRadioPoller.java
+++ develop/src/main/java/com/twitter/teruteru128/study/onion/DeepWebRadioPoller.java
@@ -1,15 +1,15 @@
-package com.twitter.teruteru128.study;
+package com.twitter.teruteru128.study.onion;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
-import java.net.SocketException;
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.concurrent.Callable;
 
-public class DeepWebRadioPoller implements Runnable {
+public class DeepWebRadioPoller implements Callable<Void> {
 
   private URL url;
   private Proxy proxy;
@@ -18,24 +18,19 @@ public class DeepWebRadioPoller implements Runnable {
     try {
       url = new URL("http://anonyradixhkgh5myfrkarggfnmdzzhhcgoy2v66uf7sml27to5n2tid.onion/");
       // url = new URL("http://jpchv3cnhonxxtzxiami4jojfnq3xvhccob5x3rchrmftrpbjjlh77qd.onion/");
-      proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("localhost", 9050));
+      proxy = new Proxy(Proxy.Type.SOCKS, InetSocketAddress.createUnresolved("localhost", 9050));
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
     }
   }
 
   @Override
-  public void run() {
-    try {
+  public Void call() throws IOException {
       var connection = (HttpURLConnection) url.openConnection(proxy);
       connection.setRequestMethod("HEAD");
       System.out.printf("[%s]access start: ", LocalDateTime.now());
       connection.connect();
       System.out.printf("ok, %d%n", connection.getResponseCode());
-    } catch (SocketException e) {
-      System.err.printf("NG: %s%n", e.getMessage());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    return null;
   }
 }
