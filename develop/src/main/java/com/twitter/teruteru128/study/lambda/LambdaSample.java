d3e4ab3d08acf7421a8aff4c73c5d3061c44d9ed
Teruteru <teruterubouzu1024@gmail.com>
2022-03-19 20:52:28 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/lambda/LambdaSample.java
+++ develop/src/main/java/com/twitter/teruteru128/study/lambda/LambdaSample.java
@@ -7,13 +7,17 @@ import java.util.stream.Stream;
 public class LambdaSample implements Callable<Void> {
 
     public Void call() {
-        Arrays.asList("list1", "list2", "list3", "list4", "list5").stream().forEach(System.out::println);;
-        String[] array = { "配列1", "配列2", "配列3", "配列4", "配列5" };
-        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        Arrays.stream(a).forEach(System.out::println);
-        Arrays.stream(array).forEach(System.out::println);
-        Stream.of(array).forEach(System.out::println);
-        Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10").mapToInt(Integer::valueOf).forEach(System.out::println);
+        Arrays.asList("list1", "list2", "list3", "list4", "list5").stream().forEach(System.out::println);
+
+        String[] stringArray = { "配列1", "配列2", "配列3", "配列4", "配列5" };
+        Arrays.stream(stringArray).forEach(System.out::println);
+        Stream.of(stringArray).forEach(System.out::println);
+
+        int[] primitiveArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Arrays.stream(primitiveArray).forEach(System.out::println);
+
+        Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10").mapToInt(Integer::valueOf)
+                .forEach(System.out::println);
         return null;
     }
 
