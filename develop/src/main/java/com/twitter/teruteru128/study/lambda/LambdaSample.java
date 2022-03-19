a2a4ee1aacba886ae0ce4131e2231e55b3889be7
Teruteru <teruterubouzu1024@gmail.com>
2022-03-19 20:25:50 +0900

    Update LambdaSample

--- develop/src/main/java/com/twitter/teruteru128/study/lambda/LambdaSample.java
+++ develop/src/main/java/com/twitter/teruteru128/study/lambda/LambdaSample.java
@@ -1,26 +1,20 @@
 package com.twitter.teruteru128.study.lambda;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
-public class LambdaSample {
+public class LambdaSample implements Callable<Void> {
 
-    public static void main(String[] args) {
-        List<String> list = Arrays.asList("list1", "list2", "list3", "list4", "list5");
+    public Void call() {
+        Arrays.asList("list1", "list2", "list3", "list4", "list5").stream().forEach(System.out::println);;
         String[] array = { "配列1", "配列2", "配列3", "配列4", "配列5" };
         int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        IntStream ints = Arrays.stream(a);
-        Stream<String> stream1 = list.stream();
-        Stream<String> stream2 = Arrays.stream(array);
-        Stream<String> stream3 = Stream.of(array);
-        Stream<String> stream4 = Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
-        ints.forEach(System.out::println);
-        stream1.forEach(System.out::println);
-        stream2.forEach(System.out::println);
-        stream3.forEach(System.out::println);
-        stream4.mapToInt(Integer::valueOf).forEach(System.out::println);
+        Arrays.stream(a).forEach(System.out::println);
+        Arrays.stream(array).forEach(System.out::println);
+        Stream.of(array).forEach(System.out::println);
+        Stream.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10").mapToInt(Integer::valueOf).forEach(System.out::println);
+        return null;
     }
 
 }
