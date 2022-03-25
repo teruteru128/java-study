3ae4404445dde289d5093f51768f27f5e8685efb
Teruteru <teruterubouzu1024@gmail.com>
2022-03-25 20:27:36 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,13 +1,10 @@
 package com.twitter.teruteru128.study;
 
-import java.time.LocalDateTime;
-import java.time.Month;
+import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-import com.twitter.teruteru128.study.sf.Shangri_laFrontierCountDown;
 
 /**
  * Main
@@ -21,16 +18,13 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         var service = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(12);
-        var future = service.schedule(new Shangri_laFrontierCountDown(), 0, TimeUnit.NANOSECONDS);
-        var mikumikuFuture = service.schedule(new MikkuMikuniShiteYanyo(), 0, TimeUnit.NANOSECONDS);
+        var future = service.schedule(()->OffsetDateTime.of(2006, 8, 16, 7, 14, 22, 9, ZoneOffset.ofHours(9)), 0, TimeUnit.NANOSECONDS);
         var shutdownFuture = service.schedule(() -> {
             System.out.println("シャットダウンします……");
             service.shutdown();
         }, 500, TimeUnit.MILLISECONDS);
-        future.get();
-        var epoch = mikumikuFuture.get();
+        var f = future.get();
+        System.out.printf("%s%n", f.getDayOfWeek());
         shutdownFuture.get();
-        System.out.printf("%1$016x, %1$d%n", epoch);
-        System.out.printf("%1$016x, %1$d%n", 0x7fffffffL);
     }
 }
