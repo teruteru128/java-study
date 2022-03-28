4c987f7278ac077b3d35f67d09c831e170be7a43
Teruteru <teruterubouzu1024@gmail.com>
2022-03-29 01:30:42 +0900

    Update NyanpassCrawler2

--- nyanpass/src/main/java/com/twitter/teruteru128/nyanpass/NyanpassCrawler2.java
+++ nyanpass/src/main/java/com/twitter/teruteru128/nyanpass/NyanpassCrawler2.java
@@ -8,69 +8,52 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import jakarta.json.Json;
 
 /**
- * 
+ * Crawler
  */
 public class NyanpassCrawler2 implements Runnable {
 
-    public NyanpassCrawler2(ScheduledExecutorService service) {
-        super();
-        this.service = service;
+    public NyanpassCrawler2() {
+        try {
+            nyanpassUrl = new URL("https://nyanpass.com/api/get_count");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
-
-    private ScheduledExecutorService service;
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.JAPAN);
 
-    private Runnable get(final URL url) {
-        return () -> {
-            try {
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.addRequestProperty("Connection", "close");
-                connection.connect();
-                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    try (var reader = Json.createReader(new BufferedReader(new InputStreamReader(connection.getInputStream())))) {
-                        var object = reader.readObject();
-                        var time = LocalDateTime.parse(object.getString("time"), FORMATTER);
-                        var count = Long.parseLong(object.getString("count"), 10);
-                        System.out.printf("%s : %s%n", time, count);
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
-    }
+    URL nyanpassUrl = null;
 
     @Override
     public void run() {
-        URL nyanpassUrl;
         try {
-            nyanpassUrl = new URL("https://nyanpass.com/api/get_count");
-            service.scheduleAtFixedRate(get(nyanpassUrl), 0, 5, TimeUnit.SECONDS);
-            Thread.sleep(1000L * 60);
-        } catch (MalformedURLException e1) {
-            e1.printStackTrace();
-            System.exit(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        } finally {
-            System.out.println("シャットダウンするのん");
-            service.shutdown();
+            HttpURLConnection connection = (HttpURLConnection) nyanpassUrl.openConnection();
+            connection.addRequestProperty("Connection", "close");
+            connection.connect();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                try (var reader = Json
+                        .createReader(new BufferedReader(new InputStreamReader(connection.getInputStream())))) {
+                    var object = reader.readObject();
+                    var time = LocalDateTime.parse(object.getString("time"), FORMATTER);
+                    var count = Long.parseLong(object.getString("count"), 10);
+                    System.out.printf("%s : %s%n", time, count);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
-        final ScheduledThreadPoolExecutor service = new ScheduledThreadPoolExecutor(2);
+        final var service = new ScheduledThreadPoolExecutor(2);
         System.out.println("にゃんぱすー。定期クロールを開始したのん！");
-        service.schedule(new NyanpassCrawler2(service), 0, TimeUnit.NANOSECONDS);
+        service.scheduleAtFixedRate(new NyanpassCrawler2(), 0, 0, TimeUnit.NANOSECONDS);
     }
 
 }
