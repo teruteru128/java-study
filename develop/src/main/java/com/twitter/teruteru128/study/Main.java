4893603fdeb083ab88fbe84b5ca1dd62f7d3c40f
Teruteru <teruterubouzu1024@gmail.com>
2022-08-26 00:09:01 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,9 +1,29 @@
 package com.twitter.teruteru128.study;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 /**
  * Main
  */
 public class Main {
+
+    private static void print(long seed) {
+        System.out.println(seed);
+        Random random = new Random(seed);
+        float f2 = random.nextFloat();
+        System.out.printf("float1: %1$f, %1$a, %2$08x%n", f2, Float.floatToIntBits(f2));
+        float f1 = random.nextFloat();
+        System.out.printf("float2: %1$f, %1$a, %2$08x%n", f1, Float.floatToIntBits(f1));
+        random.setSeed(seed);
+        double d1 = random.nextDouble();
+        double d2 = random.nextDouble();
+        System.out.printf("double1: %1$f, %1$a, %2$016x%n", d1, Double.doubleToLongBits(d2));
+        System.out.printf("double1: %1$f, %1$a, %2$016x%n", d2, Double.doubleToLongBits(d2));
+        random.setSeed(seed);
+        System.out.printf("int1: %08x%n", random.nextInt());
+        System.out.printf("int1: %08x%n", random.nextInt());
+    }
 
     /**
      * 
@@ -11,5 +31,12 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+        print(74803317123181L);
+        System.out.println("--");
+        print(125352706827826L);
+        System.out.println("--");
+        print(116229385253865L);
+        BigInteger p = BigInteger.valueOf(0xcdda5ffb215fL);
+        System.out.printf("%d%n", p.modInverse(BigInteger.valueOf(1L << 48)));
     }
 }
