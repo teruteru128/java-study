47fbfa40a077cf2bc66ad7202fb4e6a36d350fa7
Teruteru <teruterubouzu1024@gmail.com>
2022-09-16 17:00:57 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -14,13 +14,15 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        var stream = RandomGeneratorFactory.all();
-        stream.map(f->String.format("%s, %s, %b, %d", f.name(), f.group(), f.isSplittable(), f.stateBits())).forEach(System.out::println);
-        System.out.println("--");
-        System.out.println(RandomGeneratorFactory.getDefault().name());
-        System.out.println("--");
         var factory = RandomGeneratorFactory.<SecureRandom>of("SecureRandom");
         var securerandom = factory.create();
-        System.out.println(securerandom.getClass());
+        double semen = 0;
+        for (int days = 2; days < 10; days++) {
+            System.out.printf("%d日目%n", days + 1);
+            do {
+                semen = securerandom.nextDouble(1000.);
+                System.out.printf("  %fL%n", semen);
+            } while (securerandom.nextDouble() < 0.8);
+        }
     }
 }
