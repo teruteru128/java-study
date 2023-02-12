386c087eaca4f36ecab04ca030601159945b2b9c
Teruteru <teruterubouzu1024@gmail.com>
2023-02-13 02:23:31 +0900

    Add Exchanger sample

---
+++ sample/src/main/java/com/twitter/teruteru128/sample/exchange/Task.java
@@ -0,0 +1,31 @@
+package com.twitter.teruteru128.sample.exchange;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task implements Callable<Void> {
+    private Exchanger<Container> exchanger;
+    private int count;
+
+    public Task(Exchanger<Container> exchanger, int count) {
+        this.exchanger = exchanger;
+        this.count = count;
+    }
+
+    public Task(Exchanger<Container> exchanger) {
+        this(exchanger, 3);
+    }
+
+    @Override
+    public Void call() throws Exception {
+        var container = new Container((long) ThreadLocalRandom.current().nextInt(10));
+        System.out.printf("Task: %d%n", container.count);
+        for (int i = 0; i < count; i++) {
+            container = exchanger.exchange(container);
+            container.count = container.count * 10 + (long) ThreadLocalRandom.current().nextInt(10);
+            System.out.printf("Task: %d%n", container.count);
+        }
+        return null;
+    }
+}
