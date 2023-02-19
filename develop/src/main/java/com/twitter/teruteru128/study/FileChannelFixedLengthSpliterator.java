f883958fb1bcc3156cdc955a8a05f81f98549a91
Teruteru <teruterubouzu1024@gmail.com>
2023-02-19 21:18:50 +0900

    飽きた

---
+++ develop/src/main/java/com/twitter/teruteru128/study/FileChannelFixedLengthSpliterator.java
@@ -0,0 +1,32 @@
+package com.twitter.teruteru128.study;
+
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class FileChannelFixedLengthSpliterator implements Spliterator<byte[]> {
+
+    @Override
+    public boolean tryAdvance(Consumer<? super byte[]> action) {
+        return false;
+    }
+
+    @Override
+    public Spliterator<byte[]> trySplit() {
+        return null;
+    }
+
+    @Override
+    public long estimateSize() {
+        return 0;
+    }
+
+    @Override
+    public int characteristics() {
+        return 0;
+    }
+
+    void close() {
+        
+    }
+
+}
