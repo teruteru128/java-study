c901387bb80d3fb245764daaff30485d7443efac
Teruteru <teruterubouzu1024@gmail.com>
2023-02-21 23:05:20 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/bitmessage/VarintTupple.java
+++ develop/src/main/java/com/twitter/teruteru128/bitmessage/VarintTupple.java
@@ -19,38 +19,38 @@ public class VarintTupple {
 
     public static VarintTupple newInstance(byte[] data, int offset, int length) throws VarintDecodeException {
         var a = ByteBuffer.wrap(data, offset, length);
-        long value = a.get() & 0xff;
-        if (value < 253) {
-            return new VarintTupple(value, 1);
-        } else if (value == 253) {
-            if (length < 3) {
-                throw new VarintDecodeException();
-            }
-            value = a.getShort() & 0xffff;
-            if (value < 253) {
-                throw new VarintDecodeException();
-            }
-            return new VarintTupple(value, 3);
-        } else if (value == 254) {
-            if (length < 5) {
-                throw new VarintDecodeException();
-            }
-            value = a.getInt() & 0xffffffff;
-            if (value < 65536) {
-                throw new VarintDecodeException();
-            }
-            return new VarintTupple(value, 5);
-        } else if (value == 255) {
-            if (length < 9) {
-                throw new VarintDecodeException();
-            }
-            value = a.getLong() & 0xffffffffL;
-            if (value < 4294967296L) {
-                throw new VarintDecodeException();
-            }
-            return new VarintTupple(value, 9);
+        int firstByte = a.get() & 0xff;
+        long value;
+        switch (firstByte) {
+            case -3:
+                if (length < 3) {
+                    throw new VarintDecodeException();
+                }
+                value = a.getShort() & 0xffff;
+                if (value < 253) {
+                    throw new VarintDecodeException();
+                }
+                return new VarintTupple(value, 3);
+            case -2:
+                if (length < 5) {
+                    throw new VarintDecodeException();
+                }
+                value = a.getInt() & 0xffffffffL;
+                if (value < 65536) {
+                    throw new VarintDecodeException();
+                }
+                return new VarintTupple(value, 5);
+            case -1:
+                if (length < 9) {
+                    throw new VarintDecodeException();
+                }
+                value = a.getLong();
+                if (value < 4294967296L) {
+                    throw new VarintDecodeException();
+                }
+                return new VarintTupple(value, 9);
+            default:
+                return new VarintTupple(firstByte, 1);
         }
-        // ここは多分来ない……はず
-        throw new VarintDecodeException();
     }
 }
