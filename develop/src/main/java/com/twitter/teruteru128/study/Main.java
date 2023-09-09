31961e681d14e182f9bd54c97df1d61ffd4286f5
Teruteru <teruterubouzu1024@gmail.com>
2023-09-10 02:22:54 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -1,19 +1,16 @@
 package com.twitter.teruteru128.study;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.lang.reflect.InvocationTargetException;
 import java.security.Provider;
 import java.security.Security;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 /**
  * Main
  */
 public class Main {
 
-    private static final byte[] BUF = new byte[] { 0x07 };
     private static final String ORG_BOUNCYCASTLE_JCE_PROVIDER_BOUNCY_CASTLE_PROVIDER = "org.bouncycastle.jce.provider.BouncyCastleProvider";
 
     static {
@@ -35,15 +32,13 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        var service = Executors.newSingleThreadScheduledExecutor();
-        service.scheduleAtFixedRate(() -> {
-            try {
-                System.out.write(BUF);
-                System.out.flush();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }, 180, 180, TimeUnit.SECONDS);
+        var url = "jdbc:postgresql://localhost/template1";
+        var props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "postgres");
+        try (var con = DriverManager.getConnection(url, props)) {
+            System.out.println("Connected!");
+        }
     }
 
 }
