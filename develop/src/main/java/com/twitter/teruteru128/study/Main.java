2fe6dbe4bc5e7c44d57fc211277d8fe41ac554f9
Teruteru <teruterubouzu1024@gmail.com>
2024-01-06 00:29:31 +0900

    Update

--- develop/src/main/java/com/twitter/teruteru128/study/Main.java
+++ develop/src/main/java/com/twitter/teruteru128/study/Main.java
@@ -3,10 +3,8 @@ package com.twitter.teruteru128.study;
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigInteger;
 import java.security.Provider;
-import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Arrays;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Main
@@ -38,6 +36,5 @@ public class Main {
         if (args.length < 1) {
             System.exit(1);
         }
-        System.out.println("うんち！");
     }
 }
--- preview/src/main/java/com/twitter/teruteru128/preview/Main.java
+++ preview/src/main/java/com/twitter/teruteru128/preview/Main.java
@@ -1,14 +1,31 @@
 package com.twitter.teruteru128.preview;
 
 import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.SymbolLookup;
 import java.lang.foreign.ValueLayout;
 
 public class Main {
-    void main() {
-        var arena = Arena.ofAuto();
-        var seg = arena.allocate(200, 8);
-        for (long i = 0; i < 200; i += 8) {
-            seg.set(ValueLayout.JAVA_LONG, i, i);
+    void main(String[] args) throws Throwable {
+        // でも本当にほしいのはSocketとかファイルとかMessageDigestとかの連携なんだよね……
+        // もしかしてネイティブライブラリにアクセスできるならOpenSSLにアクセスもできる……？
+        // OpenCLもアクセスできるっぽい
+        var s = args.length >= 1 ? args[0] : "yattaze.";
+        try (var arena = Arena.ofConfined()) {
+            var segment = arena.allocateUtf8String(s);
+            var linker = Linker.nativeLinker();
+            var stdlib = linker.defaultLookup();
+            var strlenAddr = stdlib.find("strlen").get();
+            var strlenSig = FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS);
+
+            var strlen = linker.downcallHandle(strlenAddr, strlenSig);
+
+            System.out.printf("%d%n", (long) strlen.invokeExact(segment));
+            System.loadLibrary("OpenCL");
+            System.loadLibrary("P2P");
+            var openCL = SymbolLookup.loaderLookup();
+            var clGetPlatformIDsAddr = openCL.find("clGetPlatformIDs").get();
         }
     }
 }
