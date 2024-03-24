51bb810e2e9a85563fdfbc4fd12fc75f9be570ac
Teruteru <teruterubouzu1024@gmail.com>
2024-03-24 22:49:11 +0900

    Update

--- bitmessage/src/main/java/com/twitter/teruteru128/bitmessage/app/Spammer.java
+++ bitmessage/src/main/java/com/twitter/teruteru128/bitmessage/app/Spammer.java
@@ -1,15 +1,21 @@
 package com.twitter.teruteru128.bitmessage.app;
 
+import com.twitter.teruteru128.bitmessage.Structs;
 import com.twitter.teruteru128.bitmessage.spec.AddressFactory;
+import com.twitter.teruteru128.encode.Base58;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.*;
 import java.util.random.RandomGenerator;
 
@@ -105,7 +111,7 @@ public class Spammer {
         try (var client = HttpClient.newHttpClient()) {
             for (int i = offset; i < length; i += unitSize) {
                 var joiner = new StringJoiner(",", "[", "]");
-                var list = addresses.subList(offset, Math.min(length, offset + unitSize));
+                var list = addresses.subList(i, Math.min(length, i + unitSize));
                 for (var address : list) {
                     builder.append(address);
                     builder.append("\",\"BM-2cVPhC8Bdrx2ZemLw98oGUsgjDAfwsigyc\",\"\",\"\",2,3600],\"id\":");
@@ -119,5 +125,101 @@ public class Spammer {
                 Thread.sleep(d);
             }
         }
+    }
+
+    public static void unitSpam2(List<String> addresses, int unitSize, int offset) throws SQLException {
+        int length = addresses.size();
+        System.err.printf("addresses size: %d%n", length);
+        var sqlitePath = System.getenv("SQLITE_PATH");
+        if (sqlitePath == null || sqlitePath.isEmpty()) {
+            System.err.println("sql path not found!");
+            return;
+        }
+        try (var connection = DriverManager.getConnection("jdbc:sqlite:" + sqlitePath)) {
+            var ps = connection.prepareStatement("INSERT INTO sent VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+            // from address
+            ps.setString(4, "BM-2cVPhC8Bdrx2ZemLw98oGUsgjDAfwsigyc");
+            // subject
+            ps.setString(5, "");
+            // message
+            ps.setString(6, "");
+            // sleep till
+            ps.setLong(10, 0);
+            // status
+            ps.setString(11, "msgqueued");
+            // retry number
+            ps.setInt(12, 0);
+            // folder
+            ps.setString(13, "trash");
+            // encoding type
+            ps.setInt(14, 2);
+            // ttl
+            ps.setInt(15, 3600);
+            for (int i = offset; i < length; i += unitSize) {
+                var list = addresses.subList(i, Math.min(length, i + unitSize));
+                for (var address : list) {
+                    // msg id
+                    ps.setBytes(1, uuidToBytes(UUID.randomUUID()));
+                    // address
+                    ps.setString(2, address);
+                    // ripe
+                    var ripe = addressToRipe(address);
+                    ps.setBytes(3, ripe);
+                    // ack data
+                    ps.setBytes(7, genAckPayload(1, 1));
+                    // sent time & last action time
+                    var epochSecond = Instant.now().getEpochSecond();
+                    ps.setLong(8, epochSecond);
+                    ps.setLong(9, epochSecond);
+                    ps.addBatch();
+                }
+                System.err.printf("%s, %s%n", list.getFirst(), list.getLast());
+                System.out.println("Arrays.stream(ps.executeBatch()).sum() = " + Arrays.stream(ps.executeBatch()).sum());
+            }
+        }
+    }
+
+    private static byte[] genAckPayload(int streamNumber, int stealthLevel) {
+        int ackType;
+        int version;
+        byte[] ackData;
+        switch (stealthLevel) {
+            case 2 -> {
+                ackData = new byte[64];
+                ackType = 2;
+                version = 1;
+            }
+            case 1 -> {
+                ackData = new byte[32];
+                RANDOM.nextBytes(ackData);
+                ackType = 0;
+                version = 4;
+            }
+            default -> {
+                ackData = new byte[32];
+                RANDOM.nextBytes(ackData);
+                ackType = 2;
+                version = 1;
+            }
+        }
+        var ackTypePacket = ByteBuffer.allocate(4).putInt(ackType).flip();
+        var versionPacket = Structs.encodeVarint(version);
+        var streamNumberPacket = Structs.encodeVarint(streamNumber);
+        return ByteBuffer.allocate(ackTypePacket.limit() + versionPacket.length + streamNumberPacket.length + ackData.length).put(ackTypePacket).put(versionPacket).put(streamNumberPacket).put(ackData).array();
+    }
+
+    private static byte[] addressToRipe(String address) {
+        if (!address.startsWith("BM-")) {
+            throw new IllegalArgumentException("");
+        }
+        var encoded = Base58.decode(address.substring(3));
+        var shortedRipe = Arrays.copyOfRange(encoded, 2, encoded.length - 4);
+        var ripe = new byte[20];
+        System.arraycopy(shortedRipe, 0, ripe, 20 - shortedRipe.length, shortedRipe.length);
+        return ripe;
+    }
+
+    private static byte[] uuidToBytes(UUID uuid) {
+        return ByteBuffer.allocate(16).putLong(uuid.getMostSignificantBits()).putLong(uuid.getLeastSignificantBits()).array();
     }
 }
