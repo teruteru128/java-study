7969b8262163908d160f2a385a92ca6454d49af2
Teruteru <teruterubouzu1024@gmail.com>
2024-04-13 18:18:54 +0900

    モジュール名変更

--- foreign/src/main/java/module-info.java
+++ foreign/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.twitter.teruteru.preview {
+module com.github.teruteru.foreign {
     requires com.github.teruteru.foreign.opencl;
     requires com.github.teruteru.foreign.windows;
     requires com.github.teruteru.foreign.setjmp;
