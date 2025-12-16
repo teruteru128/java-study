2ff0d6fc665623e26f14eb8d1fb4bbc0e935a25a
Teruteru <teruterubouzu1024@gmail.com>
2025-12-17 00:26:18 +0900

    User 用空クラス作成

---
+++ sample/src/main/java/com/github/teruteru128/sample/user/UserPasskeyCredential.java
@@ -0,0 +1,5 @@
+package com.github.teruteru128.sample.user;
+
+public class UserPasskeyCredential {
+
+}
---
+++ sample/src/main/java/com/github/teruteru128/sample/user/UserPasswordCredential.java
@@ -0,0 +1,7 @@
+package com.github.teruteru128.sample.user;
+
+import java.io.Serializable;
+
+public class UserPasswordCredential implements Serializable {
+
+}
