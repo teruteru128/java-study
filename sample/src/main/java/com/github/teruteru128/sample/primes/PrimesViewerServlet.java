56c00b82bc1d4dbc77349516dac983aa363ed786
Teruteru <teruterubouzu1024@gmail.com>
2025-12-18 17:09:25 +0900

    import 文整理

--- sample/src/main/java/com/github/teruteru128/sample/primes/PrimesViewerServlet.java
+++ sample/src/main/java/com/github/teruteru128/sample/primes/PrimesViewerServlet.java
@@ -5,8 +5,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
